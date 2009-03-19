83da556a33b76ec3e67f4729b31fb347efb50584
zozoh <zozohtnt@gmail.com>
2009-03-19 02:45:17 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@18 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/com/zzh/dao/impl/OraclePager.java
+++ src/com/zzh/dao/impl/OraclePager.java
@@ -1,16 +1,11 @@
 package com.zzh.dao.impl;
 
 import com.zzh.dao.Pager;
-import com.zzh.lang.Lang;
 
 public class OraclePager extends Pager {
 
 	private static String ptn = "SELECT * FROM ("
 			+ "SELECT T.*, ROWNUM RN FROM (%%s) T WHERE ROWNUM <= %d) WHERE RN > %d";
-
-	protected OraclePager() {
-		throw Lang.makeThrow("No implement yet!!!");
-	}
 
 	@Override
 	protected String getLimitString() {
