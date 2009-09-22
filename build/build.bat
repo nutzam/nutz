1bae12211b23a468411403fac578d1d9bee8cdd4
zozoh <zozohtnt@gmail.com>
2009-09-22 04:33:12 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@205 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,9 @@
 @echo off
-SET TOMCAT_HOME=C:\Program Files\Apache Software Foundation\Tomcat 6.0
-SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_16
-SET DEPS=D:\home\nutz\deps
-SET OUTPUT=D:\home\nutz\jars
-SET PROJECT_HOME=D:\home\zozoh\workspace\svn\google.nutz\trunk
+SET TOMCAT_HOME=%TOMCAT_HOME%
+SET JAVA_HOME=%JAVA_HOME%
+SET DEPS=%DEPS%
+SET OUTPUT=%OUTPUT%
+SET PROJECT_HOME=%PROJECT_HOME%
 
 cd %PROJECT_HOME%\build
 ant
