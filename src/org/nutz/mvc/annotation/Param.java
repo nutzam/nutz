f795d64da9a80d9085956fa447e42995c8ac7fb3
zozoh <zozohtnt@gmail.com>
2009-10-22 11:00:17 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@282 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/annotation/Param.java
+++ src/org/nutz/mvc/annotation/Param.java
@@ -6,9 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target( { ElementType.PARAMETER })
+@Target( { ElementType.PARAMETER, ElementType.FIELD })
 public @interface Param {
 
 	String value();
-	
+
 }
