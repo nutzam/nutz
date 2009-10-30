96c4f316785cfd96b78a63c742394484dd0bc93c
zozoh <zozohtnt@gmail.com>
2009-10-30 13:59:19 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@340 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/dao/pager/MysqlPager.java
+++ src/org/nutz/dao/pager/MysqlPager.java
@@ -1,10 +1,11 @@
 package org.nutz.dao.pager;
 
-
 public class MysqlPager extends AbstractPager {
 
 	public String format(String sql) {
-		return null;
+		StringBuilder sb = new StringBuilder(sql);
+		sb.append(" LIMIT ").append(getOffset()).append(',').append(getPageSize());
+		return sb.toString();
 	}
 
 }
--- src/org/nutz/dao/pager/OraclePager.java
+++ src/org/nutz/dao/pager/OraclePager.java
@@ -1,10 +1,15 @@
 package org.nutz.dao.pager;
 
-
 public class OraclePager extends AbstractPager {
 
 	public String format(String sql) {
-		return null;
+		int firstRn = this.getOffset();
+		int lastRn = firstRn + this.getPageSize();
+		StringBuilder sb = new StringBuilder("SELECT * FROM (SELECT T.*, ROWNUM RN FROM (");
+		sb.append(sql);
+		sb.append(") T WHERE ROWNUM <=").append(lastRn);
+		sb.append(") WHERE RN >").append(firstRn);
+		return sb.toString();
 	}
 
 }
