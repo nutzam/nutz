60232b2407d9bde754c0338d09f93e2b241bddbe
zozoh <zozohtnt@gmail.com>
2009-11-11 12:54:17 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@376 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/ioc/AllIoc.java
+++
@@ -1,8 +0,0 @@
-package org.nutz.ioc;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses( { AllIocWithDB.class, AllIocWithoutDB.class })
-public class AllIoc {}
--- test/org/nutz/ioc/pojo/AnimalRace.java
+++
@@ -1,7 +0,0 @@
-package org.nutz.ioc.json.pojo;
-
-public enum AnimalRace {
-
-	MAMMAL, HUMAN, INSECT, REPTILES, SPIDER
-
-}
