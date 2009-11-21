4f0a6cad6c964f9e3e1b2743db96fc4ab782fdf8
zozoh <zozohtnt@gmail.com>
2009-11-21 04:25:50 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@458 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/log/AllLog.java
+++ test/org/nutz/log/AllLog.java
@@ -4,5 +4,5 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({JDKloggerAdapterTest.class, Log4jAdapterTest.class})
+@Suite.SuiteClasses({Log4jAdapterTest.class})
 public class AllLog {}
