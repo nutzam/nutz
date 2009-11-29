bc96d2b9360ce64aaa0f59c822e7a56fdb2d90c7
wendal <wendal1985@gmail.com>
2009-11-29 00:38:01 +0000

    fix CharGenerator src String
    git-svn-id: http://nutz.googlecode.com/svn/trunk@504 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/lang/random/CharGenerator.java
+++ src/org/nutz/lang/random/CharGenerator.java
@@ -4,10 +4,10 @@ package org.nutz.lang.random;
  * Generted one char
  * 
  * @author zozoh
- * 
+ * @author wendal(wendal1985@gmail.com)
  */
 public class CharGenerator {
-	private static char[] src = "1234567890_ABCDEFGHI GKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz"
+	private static final char[] src = "1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 			.toCharArray();
 
 	public static char next() {
