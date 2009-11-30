d11b19e1074d42d9a67dfa3493e1d53bef3c8eae
wendal <wendal1985@gmail.com>
2009-11-30 09:28:53 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@516 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/log/AllLog.java
+++ test/org/nutz/log/AllLog.java
@@ -3,6 +3,7 @@ package org.nutz.log;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+//可用的adapter被作为一个静态变量保存了；所以我们暂时无法在一个进程内测试多个Adapter.
 @RunWith(Suite.class)
-@Suite.SuiteClasses({Log4jAdapterTest.class})
+@Suite.SuiteClasses({Log4jAdapterTest.class}) 
 public class AllLog {}
