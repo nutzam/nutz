3a97f1d094c8323ca2b6427fff1ce3899663d6ae
wendal <wendal1985@gmail.com>
2009-12-09 00:27:32 +0000

    改为默认抛出异常,以符合原有方法的行为.修正一个严重的bug
    git-svn-id: http://nutz.googlecode.com/svn/trunk@567 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/aop/AbstractMethodInterceptor.java
+++ src/org/nutz/aop/AbstractMethodInterceptor.java
@@ -5,12 +5,12 @@ import java.lang.reflect.Method;
 /**
  * 方法拦截器的空实现
  * @author wendal(wendal1985@gmail.com)
- *
+ * @see org.nutz.aop.MethodInterceptor
  */
 public abstract class AbstractMethodInterceptor implements MethodInterceptor {
 
 	public Object afterInvoke(Object obj, Object returnObj, Method method, Object... args) {
-		return obj;
+		return returnObj;
 	}
 
 	public boolean beforeInvoke(Object obj, Method method, Object... args) {
@@ -18,11 +18,11 @@ public abstract class AbstractMethodInterceptor implements MethodInterceptor {
 	}
 
 	public boolean whenError(Throwable e, Object obj, Method method, Object... args) {
-		return false;
+		return true;
 	}
 
 	public boolean whenException(Exception e, Object obj, Method method, Object... args) {
-		return false;
+		return true;
 	}
 
 }
