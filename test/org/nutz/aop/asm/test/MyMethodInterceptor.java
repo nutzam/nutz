657765c2bcffb16b5e943f9d433f7bd63cebe78e
zozoh <zozohtnt@gmail.com>
2009-12-10 07:12:42 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@574 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/aop/asm/test/MyMethodInterceptor.java
+++ test/org/nutz/aop/asm/test/MyMethodInterceptor.java
@@ -7,15 +7,12 @@ import org.nutz.castor.Castors;
 
 public class MyMethodInterceptor implements MethodInterceptor {
 
-	@Override
-	public Object afterInvoke(Object obj, Object result, Method arg2,
-			Object... objs) {
+	public Object afterInvoke(Object obj, Object result, Method arg2, Object... objs) {
 		System.out.println("After..... " + arg2.getName());
 		printArgs(objs);
 		return result;
 	}
 
-	@Override
 	public boolean beforeInvoke(Object arg0, Method arg1, Object... objs) {
 		System.out.println("-----------------------------------------------------");
 		System.out.println("Before.... " + arg1.getName());
@@ -23,24 +20,20 @@ public class MyMethodInterceptor implements MethodInterceptor {
 		return true;
 	}
 
-	@Override
-	public boolean whenError(Throwable e, Object arg1, Method arg2,
-			Object... objs) {
-		System.out.println("抛出了Throwable "+e);
+	public boolean whenError(Throwable e, Object arg1, Method arg2, Object... objs) {
+		System.out.println("抛出了Throwable " + e);
 		printArgs(objs);
 		return true;
 	}
 
-	@Override
-	public boolean whenException(Exception e, Object arg1, Method arg2,
-			Object... objs) {
-		System.out.println("抛出了Exception "+e);
+	public boolean whenException(Exception e, Object arg1, Method arg2, Object... objs) {
+		System.out.println("抛出了Exception " + e);
 		printArgs(objs);
 		return true;
 	}
-	
-	void printArgs(Object...objs){
-		System.out.println("参数 "+Castors.me().castToString(objs));
+
+	void printArgs(Object... objs) {
+		System.out.println("参数 " + Castors.me().castToString(objs));
 	}
 
 }
