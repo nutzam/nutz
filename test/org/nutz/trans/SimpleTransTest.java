13ccfffc4d38f777e3641e55dabe54affa6e1606
amosleaf <amosleaf@gmail.com>
2009-12-14 08:11:38 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@607 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/trans/SimpleTransTest.java
+++ test/org/nutz/trans/SimpleTransTest.java
@@ -35,4 +35,31 @@ public class SimpleTransTest extends DaoCase {
 		assertTrue(xb.getId() > 0);
 	}
 
+	@Test
+	public void test_batch_update_rollback() {
+		final Cat cat1 = dao.fetch(Cat.class, "xb");
+		final Cat cat2 = new Cat();
+		cat2.setId(cat1.getId() + 1);
+		cat2.setMaster(cat1.getMaster());
+		cat2.setMasterId(cat1.getMasterId());
+		cat2.setName("xb2");
+		dao.insert(cat2);
+		assertTrue(dao.fetch(Cat.class, "xb2").getName().equals("xb2"));
+		try {
+			Trans.exec(new Atom() {
+				@Override
+				public void run() {
+					cat1.setName("p1");
+					dao.update(cat1);
+					cat2.setName("p2");
+					dao.update(cat2);
+					throw Lang.makeThrow("Quite!!!");
+				}
+			});
+		} catch (Exception e) {}
+		assertTrue(dao.fetch(Cat.class, "xb").getName().equals("xb"));
+		assertTrue(dao.fetch(Cat.class, "xb2").getName().equals("xb2"));
+		dao.delete(cat2);
+	}
+
 }
