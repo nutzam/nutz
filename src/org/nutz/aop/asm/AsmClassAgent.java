96c936c7ba5d76e917845509dfd2a0a9bb9d9693
zozoh <zozohtnt@gmail.com>
2009-12-16 14:38:36 +0000

    做了一个 quickly & dirty 的修改。
    git-svn-id: http://nutz.googlecode.com/svn/trunk@647 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/aop/asm/AsmClassAgent.java
+++ src/org/nutz/aop/asm/AsmClassAgent.java
@@ -10,28 +10,39 @@ import org.nutz.aop.asm.org.asm.Opcodes;
 
 /**
  * <b>本实现基于ASM 3.2</b>
- * <p/>相对父类的method,子类的method的方法签名会有所改变
+ * <p/>
+ * 相对父类的method,子类的method的方法签名会有所改变
  * <li>只有访问控制符public protected 和缺省
  * <li>native等标识符无法被继承.
- * <p/>若使用空的拦截器,如new AbstractMethodInterceptor(){},则子类的行为不变.
- * <p/><b>提醒:生成的Class默认为Java 1.6的类</b>
- * <p/><b>如果需要运行在Java 1.5上,请修好CLASS_LEVEL后编译. 该行为未被验证!</b>
+ * <p/>
+ * 若使用空的拦截器,如new AbstractMethodInterceptor(){},则子类的行为不变.
+ * <p/>
+ * <b>提醒:生成的Class默认为Java 1.6的类</b>
+ * <p/>
+ * <b>如果需要运行在Java 1.5上,请修好CLASS_LEVEL后编译. 该行为未被验证!</b>
+ * 
  * @author wendal(wendal1985@gmail.com)
  * @see org.nutz.aop.AbstractClassAgent
  * @see org.nutz.aop.AbstractMethodInterceptor
  */
 public class AsmClassAgent extends AbstractClassAgent {
-	
-	protected static final GeneratorClassLoader generatorClassLoader = new GeneratorClassLoader();
-	
+
+	private static GeneratorClassLoader generatorClassLoader = new GeneratorClassLoader();
+
+	public static void resetClassLoader() {
+		generatorClassLoader = new GeneratorClassLoader();
+	}
+
 	public static final int CLASS_LEVEL = Opcodes.V1_6;
 
 	@SuppressWarnings("unchecked")
-	protected <T> Class<T> generate(Pair2 [] pair2s,String newName,Class<T> klass,Constructor<T> [] constructors) {
+	protected <T> Class<T> generate(Pair2[] pair2s,
+									String newName,
+									Class<T> klass,
+									Constructor<T>[] constructors) {
 		try {
 			return (Class<T>) generatorClassLoader.loadClass(newName);
-		} catch (ClassNotFoundException e3) {
-		}
+		} catch (ClassNotFoundException e3) {}
 		Method[] methodArray = new Method[pair2s.length];
 		List<MethodInterceptor>[] methodInterceptorList = new List[pair2s.length];
 		for (int i = 0; i < pair2s.length; i++) {
@@ -39,9 +50,10 @@ public class AsmClassAgent extends AbstractClassAgent {
 			methodArray[i] = pair2.method;
 			methodInterceptorList[i] = pair2.listeners;
 		}
-		Class<T> newClass = (Class<T>)generatorClassLoader.defineClassFromClassFile(newName, ClassX.enhandClass(klass, newName, methodArray,constructors));
+		Class<T> newClass = (Class<T>) generatorClassLoader.defineClassFromClassFile(newName,
+				ClassX.enhandClass(klass, newName, methodArray, constructors));
 		AopToolkit.injectFieldValue(newClass, methodArray, methodInterceptorList);
-		return newClass ;
+		return newClass;
 	}
 
 }
--- test/org/nutz/aop/asm/RegexMethodMatcherTest.java
+++ test/org/nutz/aop/asm/RegexMethodMatcherTest.java
@@ -12,13 +12,13 @@ import org.nutz.aop.asm.test.Aop1;
 import org.nutz.lang.Mirror;
 
 public class RegexMethodMatcherTest {
-	
+
 	@Test
 	public void testRegexMethodMatcherStringStringInt() throws Throwable {
+		AsmClassAgent.resetClassLoader();
 		AsmClassAgent agent = new AsmClassAgent();
 		MyL interceptor = new MyL();
-		agent.addListener(new RegexMethodMatcher(null,"nonArgsVoid",0), interceptor);
-		
+		agent.addListener(new RegexMethodMatcher(null, "nonArgsVoid", 0), interceptor);
 		Mirror<Aop1> mirror = Mirror.me(agent.define(Aop1.class));
 		Aop1 aop1 = mirror.born("Nutz");
 		aop1.nonArgsVoid();
@@ -27,14 +27,12 @@ public class RegexMethodMatcherTest {
 		assertTrue(interceptor.runned);
 	}
 
-	
 }
 
+class MyL extends AbstractMethodInterceptor {
 
-class MyL extends AbstractMethodInterceptor{
-	
 	public boolean runned = false;
-	
+
 	@Override
 	public boolean beforeInvoke(Object obj, Method method, Object... args) {
 		runned = true;
