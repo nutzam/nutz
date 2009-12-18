6f6557049db3ace3814a1e719bb0cbce68c6e55a
zozoh <zozohtnt@gmail.com>
2009-12-18 10:57:58 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@658 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/dao/entity/annotation/Column.java
+++ src/org/nutz/dao/entity/annotation/Column.java
@@ -14,9 +14,12 @@ import java.lang.annotation.Target;
  * &#064;Column(&quot;数据库字段名&quot;)
  * </pre>
  * 
- * <b style=color:red>需要说明的是</b>
- * <p>
- * 如果你的 POJO <u><b>没有任何一个字段</b></u> 声明了这个注解，那么所有的 Java 字段都会被认为是数据库字段
+ * <b style=color:red>需要说明的是：</b>
+ * <ul>
+ * <li>
+ * 如果你的 POJO <u><b>没有任何一个字段</b></u> 声明了这个注解，那么所有的 Java 字段都会被认为是数据库字段。
+ * <li>声明了 '@Id' 和 '@Name' 的字段没必要声明这个注解。
+ * </ul>
  * 
  * @author zozoh(zozohtnt@gmail.com)
  */
