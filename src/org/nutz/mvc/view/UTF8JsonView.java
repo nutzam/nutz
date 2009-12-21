9795445ad5bde3d24b8d9e9f40857637d1309dd3
wendal <wendal1985@gmail.com>
2009-12-21 14:13:12 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@700 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/view/UTF8JsonView.java
+++ src/org/nutz/mvc/view/UTF8JsonView.java
@@ -11,7 +11,7 @@ import org.nutz.mvc.View;
 
 /**
  * 将数据采用json方式输出的试图实现
- * 
+ * @author ming300(ming300@gmail.com)
  */
 public class UTF8JsonView implements View {
 
@@ -27,21 +27,16 @@ public class UTF8JsonView implements View {
 		this.format = format;
 	}
 
-	/**
-	 *
-	 */
 	public void render(HttpServletRequest req, HttpServletResponse resp, Object obj)
 			throws IOException {
 		// by mawm 改为直接采用resp.getWriter()的方式直接输出!
 		resp.setHeader("Cache-Control", "no-cache");
 		resp.setContentType("text/json;charset=UTF-8");
-
-		// Writer writer = new BufferedWriter(new
-		// OutputStreamWriter(resp.getOutputStream(), "UTF-8"));
+		
 		if (null == obj)
 			obj = data;
 		Json.toJson(resp.getWriter(), obj, format);
-		// writer.close();
+		
 		resp.flushBuffer();
 	}
 }
