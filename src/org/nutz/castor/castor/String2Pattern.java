87a3c45feee76050a556f80761688344a9819ee7
zozoh <zozohtnt@gmail.com>
2010-01-18 11:23:26 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@825 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ src/org/nutz/castor/castor/String2Pattern.java
@@ -0,0 +1,20 @@
+package org.nutz.castor.castor;
+
+import java.util.regex.Pattern;
+
+import org.nutz.castor.Castor;
+import org.nutz.castor.FailToCastObjectException;
+
+public class String2Pattern extends Castor<String, Pattern> {
+
+	@Override
+	public Pattern cast(String src, Class<?> toType, String... args)
+			throws FailToCastObjectException {
+		try {
+			return Pattern.compile(src);
+		} catch (Exception e) {
+			throw new FailToCastObjectException("Error regex: " + src, e);
+		}
+	}
+
+}
