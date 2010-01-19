fa65c6418f5a8ad30fdf0dc45b9cff0c21b2abfd
zozoh <zozohtnt@gmail.com>
2010-01-19 19:31:29 +0000

    for Issue 111
    git-svn-id: http://nutz.googlecode.com/svn/trunk@832 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/ioc/loader/json/JsonLoader.java
+++ src/org/nutz/ioc/loader/json/JsonLoader.java
@@ -1,6 +1,7 @@
 package org.nutz.ioc.loader.json;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,15 +28,41 @@ public class JsonLoader extends MapLoader {
 		this.setMap(map);
 	}
 
-	public JsonLoader(String... files) {
+	public JsonLoader(String... paths) {
 		Map<String, Map<String, Object>> map = new HashMap<String, Map<String, Object>>();
-		for (String path : files) {
-			File f = Files.findFile(path);
-			if (null == f)
-				throw Lang.makeThrow("Fail to find file '%s'!", path);
-			map.putAll(buildMap(f));
+		File[] fs = new File[paths.length];
+		// 解析路径
+		for (int i = 0; i < fs.length; i++) {
+			fs[i] = Files.findFile(paths[i]);
+			if (null == fs[i])
+				throw Lang.makeThrow("Fail to find file '%s'!", paths[i]);
 		}
+		load(map, fs);
 		this.setMap(map);
+	}
+
+	private void load(Map<String, Map<String, Object>> map, File[] files) {
+		for (File f : files) {
+			// 如果是目录，读取内部所有 .json 和 .js 文件
+			if (f.isDirectory()) {
+				loadFromDir(map, f);
+			}
+			// 如果是文件，加载其内容
+			else if (f.isFile()) {
+				map.putAll(buildMap(f));
+			}
+		}
+	}
+
+	private void loadFromDir(Map<String, Map<String, Object>> map, File dir) {
+		File[] files = dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				if (name.endsWith(".js") || name.endsWith(".json"))
+					return true;
+				return false;
+			}
+		});
+		load(map, files);
 	}
 
 	private Map<String, Map<String, Object>> buildMap(File f) {
