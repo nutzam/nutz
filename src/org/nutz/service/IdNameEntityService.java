8d7e195b66b615c5778aaca4f85fb9fc5fc5b8a1
zozoh <zozohtnt@gmail.com>
2010-03-05 11:22:46 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@907 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/service/IdNameEntityService.java
+++ src/org/nutz/service/IdNameEntityService.java
@@ -21,8 +21,16 @@ public abstract class IdNameEntityService<T> extends IdEntityService<T> {
 	public T fetch(String name) {
 		return dao().fetch(getEntityClass(), name);
 	}
-	
-	public boolean exists(String  name) {
+
+	public T smartFetch(String str) {
+		try {
+			long id = Long.parseLong(str);
+			return fetch(id);
+		} catch (Exception e) {}
+		return fetch(str);
+	}
+
+	public boolean exists(String name) {
 		EntityField ef = getEntity().getNameField();
 		if (null == ef)
 			return false;
