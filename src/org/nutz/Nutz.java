7b30d7002c3f8891c99772561f22e0a78baccfa9
zozoh <zozohtnt@gmail.com>
2010-03-09 08:40:56 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@912 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/Nutz.java
+++ src/org/nutz/Nutz.java
@@ -2,11 +2,14 @@ package org.nutz;
 
 /**
  * 仅用于识别当前版本号
+ * 
  * @author Wendal(wendal1985@gmail.com)
- *
+ * 
  */
-public final class NutzVersion {
+public final class Nutz {
 
-	public static final String VERSION = "1.a.27";
-	
+	public static String version() {
+		return "1.a.27";
+	}
+
 }
