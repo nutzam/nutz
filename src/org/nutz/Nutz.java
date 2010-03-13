39b2cde3228c4826a1dedf93bb21c530d9bb111f
wendal <wendal1985@gmail.com>
2010-03-13 10:38:05 +0000

    添加 License.txt
    git-svn-id: http://nutz.googlecode.com/svn/trunk@941 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/Nutz.java
+++ src/org/nutz/Nutz.java
@@ -1,7 +1,10 @@
 package org.nutz;
 
 /**
- * 仅用于识别当前版本号
+ *	用于识别当前版本号和版权声明!
+ * <br/>
+ * Nutz is Licensed under the Apache License, Version 2.0 (the "License")
+ * <p/>http://www.apache.org/licenses/LICENSE-2.0
  * 
  * @author Wendal(wendal1985@gmail.com)
  * 
