08b382facc9c8021ba01d6b1e773d94f0c467bfb
amosleaf <amosleaf@gmail.com>
2010-03-31 06:38:47 +0000

    add the uploadTest case to mvc/AllMvc.java
    git-svn-id: http://nutz.googlecode.com/svn/trunk@995 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/mvc/AllMvc.java
+++ test/org/nutz/mvc/AllMvc.java
@@ -3,7 +3,8 @@ package org.nutz.mvc;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.nutz.mvc.init.PathNodeTest;
+import org.nutz.mvc.upload.UploadingTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({PathNodeTest.class})
+@Suite.SuiteClasses({PathNodeTest.class,UploadingTest.class})
 public class AllMvc {}
