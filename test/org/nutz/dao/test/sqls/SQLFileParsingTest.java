3c78b0fde5eaa175aa8477555dabec3a03643c64
zozoh <zozohtnt@gmail.com>
2010-04-22 03:44:58 +0000

    fix issue 160
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1109 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/dao/test/sqls/SQLFileParsingTest.java
+++ test/org/nutz/dao/test/sqls/SQLFileParsingTest.java
@@ -17,11 +17,9 @@ public class SQLFileParsingTest {
 
 	private static final String PATH = "org/nutz/dao/test/sqls/sqls.sqls";
 
-	private SqlManager sqls;
-
 	@Test
 	public void check_Count_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		assertEquals(10, sqls.count());
 		String[] keys = {	".abc.drop",
 							".abc.create",
@@ -38,42 +36,42 @@ public class SQLFileParsingTest {
 
 	@Test
 	public void check_Create_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		Sql sql = sqls.create(".abc.create");
 		assertTrue(sql.toString().toUpperCase().startsWith("CREATE"));
 	}
 
 	@Test
 	public void check_Insert_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		Sql sql = sqls.create(".abc.insert");
 		assertTrue(sql.toString().toUpperCase().startsWith("INSERT"));
 	}
 
 	@Test
 	public void check_Update_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		Sql sql = sqls.create(".abc.update");
 		assertTrue(sql.toString().toUpperCase().startsWith("UPDATE"));
 	}
 
 	@Test
 	public void check_Fetch_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		Sql sql = sqls.create("abc.fetch");
 		assertTrue(sql.toString().toUpperCase().startsWith("SELECT"));
 	}
 
 	@Test
 	public void check_Query_SQL() {
-		sqls = new FileSqlManager(PATH);
+		SqlManager sqls = new FileSqlManager(PATH);
 		Sql sql = sqls.create("abc.query");
 		assertTrue(sql.toString().toUpperCase().startsWith("SELECT"));
 	}
 
 	@Test
 	public void check_PersonTestSQLs() {
-		sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
+		SqlManager sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
 		String[] keys = {	".abc.drop",
 							".abc.create",
 							".abc.insert",
@@ -91,18 +89,28 @@ public class SQLFileParsingTest {
 
 	@Test
 	public void check_parse_comboSqls() {
-		sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
+		SqlManager sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
 		ComboSql sql = sqls.createCombo();
 		assertEquals(10, sql.count());
 	}
 
 	@Test
 	public void test_sqls_save() throws IOException {
-		sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
+		SqlManager sqls = new FileSqlManager("org/nutz/dao/test/sqls/sqls.sqls");
 		int count = sqls.count();
 		File f = Files.findFile("org/nutz/dao/test/sqls/save.sqls");
 		((FileSqlManager) sqls).saveAs(f.getAbsolutePath());
 		sqls = new FileSqlManager("org/nutz/dao/test/sqls/save.sqls");
 		assertEquals(count, sqls.count());
+	}
+
+	@Test
+	public void test_parse_whole_directory() {
+		SqlManager sqls = new FileSqlManager("org/nutz/dao/test/sqls/dir");
+		assertTrue(sqls.count() > 0);
+		Sql sql = sqls.create(".abc.update");
+		sql.params().set("name", "ABC");
+		sql.params().set("id", 16);
+		assertEquals("UPDATE t_abc SET name='ABC' WHERE id=16;", sql.toString());
 	}
 }
