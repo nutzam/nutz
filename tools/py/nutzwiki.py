ec16e04825aa2dadce3555c316174b5a122466ce
zozoh <zozohtnt@gmail.com>
2010-04-29 10:43:42 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@1161 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ tools/py/nutzwiki.py
@@ -0,0 +1,23 @@
+# define the variable
+wiki_src = "";
+wiki_src_en = "";
+tmp = "";
+wiki_dest = "";
+
+# copy wiki file to source
+
+# copy en-wiki file to source
+
+# copy images / css / html / xml to en-wiki
+
+# parse the index.xml in en-wiki, and change its title
+
+# process the zdoc for wiki-target
+
+# process the zdoc for en-wiki-target
+
+# copy the wiki to svn-wiki
+
+# copy the wiki to svn-wiki-en
+
+print "ddddd";
