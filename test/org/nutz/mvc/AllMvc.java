4572750219064e7c536a676f58ac0d447ea6a8f7
zozoh <zozohtnt@gmail.com>
2010-05-02 15:27:34 +0000

    Attach the JsonAdaptorTest to AllMvc
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1181 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/mvc/AllMvc.java
+++ test/org/nutz/mvc/AllMvc.java
@@ -3,6 +3,7 @@ package org.nutz.mvc;
 import org.junit.runner.RunWith;
 
 import org.junit.runners.Suite;
+import org.nutz.mvc.adaptor.JsonAdaptorTest;
 import org.nutz.mvc.init.MvcBaseTest;
 import org.nutz.mvc.init.PathNodeTest;
 import org.nutz.mvc.upload.unit.UploadingUnitTest;
@@ -12,5 +13,6 @@ import org.nutz.mvc.upload.util.BufferRingTest;
 @Suite.SuiteClasses({	PathNodeTest.class,
 						MvcBaseTest.class,
 						BufferRingTest.class,
-						UploadingUnitTest.class})
+						UploadingUnitTest.class,
+						JsonAdaptorTest.class})
 public class AllMvc {}
