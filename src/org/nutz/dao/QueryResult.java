5ca43c05d25a417484cbbcaff686bbcc53d11f0a
zozoh <zozohtnt@gmail.com>
2010-05-05 16:36:45 +0000

    ...
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1187 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/dao/QueryResult.java
+++ src/org/nutz/dao/QueryResult.java
@@ -1,7 +1,9 @@
 package org.nutz.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.nutz.castor.Castors;
 import org.nutz.dao.pager.Pager;
 
 public class QueryResult {
@@ -16,6 +18,24 @@ public class QueryResult {
 
 	public List<?> getList() {
 		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> List<T> getList(Class<T> eleType) {
+		return (List<T>) list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> List<T> convertList(Class<T> eleType){
+		if(null==list || list.isEmpty())
+			return (List<T>)list;
+		
+		List<T> re = new ArrayList<T>(list.size());
+		Castors castors = Castors.me();
+		for(Object obj : list) 
+			re.add(castors.castTo(obj, eleType));
+	
+		return re;
 	}
 
 	public void setList(List<?> list) {
