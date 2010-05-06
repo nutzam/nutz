cb38f7e7afe57ea51ad1f3fb4094eaac586eb5c9
zozoh <zozohtnt@gmail.com>
2010-05-06 02:44:50 +0000

    static  没必要
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1188 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/castor/DefaultCastorSetting.java
+++ src/org/nutz/castor/DefaultCastorSetting.java
@@ -6,7 +6,7 @@ import org.nutz.castor.castor.DateTimeCastor;
 
 class DefaultCastorSetting {
 
-	public static void setup(DateTimeCastor<?, ?> c) {
+	public void setup(DateTimeCastor<?, ?> c) {
 		c.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
 		c.setTimeFormat(new SimpleDateFormat("HH:mm:ss"));
 		c.setDateTimeFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
