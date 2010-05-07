4a3b848736082b798aa48c1c87c68a90b2b7ba1a
wendal <wendal1985@gmail.com>
2010-05-07 15:55:32 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@1195 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ src/org/nutz/mvc/init/AnnotationIocProvider.java
@@ -0,0 +1,17 @@
+package org.nutz.mvc.init;
+
+import javax.servlet.ServletConfig;
+
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.impl.ScopeContext;
+import org.nutz.ioc.loader.annotation.AnnotationIocLoader;
+import org.nutz.mvc.IocProvider;
+
+public class AnnotationIocProvider implements IocProvider {
+
+	public Ioc create(ServletConfig config, String[] args) {
+		return new NutIoc(new AnnotationIocLoader(args), new ScopeContext("app"), "app");
+	}
+
+}
