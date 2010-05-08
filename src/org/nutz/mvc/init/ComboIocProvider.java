8cee9e03e96b3cb878b612fc969a3048fcbab694
wendal <wendal1985@gmail.com>
2010-05-08 09:30:15 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@1203 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ src/org/nutz/mvc/init/ComboIocProvider.java
@@ -0,0 +1,23 @@
+package org.nutz.mvc.init;
+
+import javax.servlet.ServletConfig;
+
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.impl.ScopeContext;
+import org.nutz.ioc.loader.combo.ComboIocLoader;
+import org.nutz.lang.Lang;
+import org.nutz.mvc.IocProvider;
+
+public class ComboIocProvider implements IocProvider {
+
+	public Ioc create(ServletConfig config, String[] args) {
+		try {
+			return new NutIoc(new ComboIocLoader(args), new ScopeContext("app"), "app");
+		}
+		catch (ClassNotFoundException e) {
+			throw Lang.wrapThrow(e);
+		}
+	}
+
+}
