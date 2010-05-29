f5f6fe08c77e2c1d3a8327f15036e1b6a4106911
wendal <wendal1985@gmail.com>
2010-05-29 11:51:32 +0000

    移除Http里面的main方法.
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1277 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/http/Http.java
+++ src/org/nutz/http/Http.java
@@ -9,16 +9,6 @@ import org.nutz.lang.Encoding;
 
 public class Http {
 
-	public static void main(String[] args) {
-		// JSESSIONID=A77F5D4960669B7375EFFB081A8CC9C7
-		String url = "http://localhost:8888/test/c/d?A=123";
-		Request request = Request.create(url, Request.METHOD.GET, "{id:12,tt:66}");
-		request.setCookie(new Cookie("JSESSIONID=5F081DC2E0F1780EBA88566E8D605EA4"));
-		Response re = Sender.create(request).send();
-		System.out.println(re.getHeader().toString());
-		System.out.println(re.getContent());
-	}
-
 	public static class multipart {
 		public static String getBoundary(String contentType) {
 			if (null == contentType)
