fcbba7a058fb980c7c0c1e265949176272661882
zozoh <zozohtnt@gmail.com>
2010-06-20 05:25:33 +0000

    ...
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1351 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/mvc/upload/unit/UploadingUnitTest.java
+++ test/org/nutz/mvc/upload/unit/UploadingUnitTest.java
@@ -15,7 +15,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.nutz.filepool.FilePool;
 import org.nutz.filepool.NutFilePool;
-import org.nutz.lang.Dumps;
 import org.nutz.lang.Files;
 import org.nutz.lang.Streams;
 import org.nutz.mock.Mock;
