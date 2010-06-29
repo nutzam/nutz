2bbc72effec07b18bbda3395ba61e55d2da46531
wendal <wendal1985@gmail.com>
2010-06-29 14:38:58 +0000

    git-svn-id: http://nutz.googlecode.com/svn/trunk@1382 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/resource/impl/WebResourceScan.java
+++ src/org/nutz/resource/impl/WebResourceScan.java
@@ -16,23 +16,30 @@ import java.util.zip.ZipFile;
 import javax.servlet.ServletContext;
 
 import org.nutz.resource.NutResource;
+import org.nutz.resource.ResourceScan;
 
-public class WebResourceScan extends AbstractResourceScan {
-	
+/**
+ * 只适用于标准的Web结构的资源扫描
+ * 
+ * @author Wendal(wendal1985@gmail.com)
+ * 
+ */
+@SuppressWarnings("unchecked")
+public class WebResourceScan implements ResourceScan {
+
 	private static final String WebClasses = "/WEB-INF/classes";
 
 	private static final String WebLib = "/WEB-INF/lib";
-	
+
 	private ServletContext servletContext;
-	
+
 	public WebResourceScan(ServletContext servletContext) {
 		this.servletContext = servletContext;
 	}
 
-	@SuppressWarnings("unchecked")
 	public List<NutResource> list(String src, String filter) {
 		List<NutResource> list = new ArrayList<NutResource>();
-		//获取全部jar
+		// 获取全部jar
 		Set<String> jars = servletContext.getResourcePaths(WebLib);
 		for (String path : jars) {
 			try {
@@ -46,14 +53,16 @@ public class WebResourceScan extends AbstractResourceScan {
 						list.add(resource);
 					}
 				}
-			}catch (Throwable e) {
+			}
+			catch (Throwable e) {
 				e.printStackTrace();
 			}
 		}
-		//获取classes里面文件
+		// 获取classes里面文件
 		Set<String> set = lists(WebClasses);
 		for (String filePath : set) {
-			String simpleName = filePath.substring(filePath.lastIndexOf(WebClasses) + WebClasses.length());
+			String simpleName = filePath.substring(filePath.lastIndexOf(WebClasses)
+													+ WebClasses.length());
 			if (simpleName.startsWith(src) && simpleName.endsWith(filter)) {
 				FileResource resource = new FileResource(filePath, simpleName);
 				list.add(resource);
@@ -65,13 +74,12 @@ public class WebResourceScan extends AbstractResourceScan {
 	public boolean canWork() {
 		return false;
 	}
-	
-	@SuppressWarnings("unchecked")
-	private Set<String> lists(String path){
+
+	private Set<String> lists(String path) {
 		Set<String> set = new HashSet<String>();
 		if (path.endsWith("/"))
 			set.add(path);
-		else{
+		else {
 			Set<String> pps = servletContext.getResourcePaths(path);
 			if (pps != null && pps.size() > 0)
 				for (String str : pps)
@@ -82,29 +90,23 @@ public class WebResourceScan extends AbstractResourceScan {
 
 	static class JarEntryResource extends NutResource {
 
-		private String jarEntryName;
-
 		private String jarFileName;
 
 		public JarEntryResource(String jarFileName, String jarEntryName) {
 			super();
 			this.jarFileName = jarFileName;
-			this.jarEntryName = jarEntryName;
+			this.name = jarEntryName;
 		}
 
 		@Override
 		public InputStream getInputStream() throws IOException {
 			ZipFile zipFile = new ZipFile(jarFileName);
-			ZipEntry zipEntry = zipFile.getEntry(jarEntryName);
+			ZipEntry zipEntry = zipFile.getEntry(name);
 			return zipFile.getInputStream(zipEntry);
 		}
 
-		@Override
-		public String getName() {
-			return this.jarEntryName;
-		}
 	}
-	
+
 	static class FileResource extends NutResource {
 
 		private String filePath;
