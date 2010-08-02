2da99060022e4cb9881bdd13153553890ef6f939
wendal <wendal1985@gmail.com>
2010-08-02 14:31:45 +0000

    移除AbstractResourceScan里面的注释,AbstractResourceScan这个类是否还有存在的必要??
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1452 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/resource/impl/AbstractResourceScan.java
+++ src/org/nutz/resource/impl/AbstractResourceScan.java
@@ -4,39 +4,4 @@ import org.nutz.plugin.Plugin;
 import org.nutz.resource.ResourceScan;
 
 public abstract class AbstractResourceScan implements ResourceScan, Plugin {
-
-	// TODO 删除下面的注释
-	/*
-	zzh: 下面的两个函数不再有用了
-	
-	protected String getClassPath() {
-		Properties properties = System.getProperties();
-		if (properties != null)
-			for (Entry<Object, Object> entry : properties.entrySet())
-				if (entry.getValue() != null
-					&& "java.class.path".equalsIgnoreCase(entry.getKey().toString()))
-					return entry.getValue().toString();
-		Map<String, String> env = System.getenv();
-		if (env != null)
-			for (Entry<String, String> entry : env.entrySet())
-				if ("CLASSPATH".equalsIgnoreCase(entry.getKey()))
-					return entry.getValue();
-		return null;
-	}
-
-	protected String[] splitedClassPath() {
-		String CLASSPATH = getClassPath();
-		if (CLASSPATH != null) {
-			Object pathSeparator = System.getProperties().get("path.separator");
-			if (pathSeparator == null) {
-				if (Lang.isWin())
-					pathSeparator = ";";
-				else
-					pathSeparator = ":";
-			}
-			return CLASSPATH.split(pathSeparator.toString());
-		}
-		return null;
-	}
-	*/
 }
