aab85b617a7e697961d3c027d10a7da184e5b38e
wendal <wendal1985@gmail.com>
2010-09-15 06:54:52 +0000

    FileInjector获取File字段前判断是否为文件,以避免抛ClassCastException
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1532 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/upload/injector/FileInjector.java
+++ src/org/nutz/mvc/upload/injector/FileInjector.java
@@ -5,10 +5,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
 import org.nutz.mvc.adaptor.ParamInjector;
 import org.nutz.mvc.upload.TempFile;
 
 public class FileInjector implements ParamInjector {
+	
+	private static final Log LOG = Logs.getLog(FileInjector.class);
 
 	public FileInjector(String name) {
 		this.name = name;
@@ -18,7 +22,13 @@ public class FileInjector implements ParamInjector {
 
 	@SuppressWarnings("unchecked")
 	public Object get(HttpServletRequest req, HttpServletResponse resp, Object refer) {
-		TempFile temp = (TempFile) ((Map<String, Object>) refer).get(name);
-		return temp == null ? null : temp.getFile();
+		Object obj = ((Map<String, Object>) refer).get(name);
+		if (obj == null)
+			return null;
+		if (obj instanceof TempFile)
+			return ((TempFile)obj).getFile();
+		if (LOG.isWarnEnabled())
+			LOG.warn("Form Field isn't a File!!");
+		throw new RuntimeException("Form Field isn't a File!!");
 	}
 }
