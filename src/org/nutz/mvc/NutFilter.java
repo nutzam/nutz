68ea1171c1459b502922444ff2c549a4b3c2eca5
zozoh <zozohtnt@gmail.com>
2010-10-30 16:22:39 +0000

    Fix issue 301
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1629 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/NutFilter.java
+++ src/org/nutz/mvc/NutFilter.java
@@ -18,6 +18,11 @@ import org.nutz.log.Logs;
 import org.nutz.mvc.init.Inits;
 import org.nutz.mvc.init.config.FilterNutConfig;
 
+/**
+ * 同 JSP/Serlvet 容器的挂接点
+ * 
+ * @author zozoh(zozohtnt@gmail.com)
+ */
 public class NutFilter implements Filter {
 
 	private static final String IGNORE = "^.+\\.(jsp|png|gif|jpg|js|css|jspx|jpeg|swf)$";
@@ -32,13 +37,18 @@ public class NutFilter implements Filter {
 
 	public void init(FilterConfig conf) throws ServletException {
 		config = new FilterNutConfig(conf);
-		Loading ing = Inits.init(config, true);
-		if (null != ing)
-			urls = ing.getUrls();
+		// 如果仅仅是用来更新 Message 字符串的，不加载 Nutz.Mvc 设定
+		// @see Issue 301
+		String skipMode = Strings.sNull(conf.getInitParameter("skip-mode"), "false").toLowerCase();
+		if (!"true".equals(skipMode)) {
+			Loading ing = Inits.init(config, true);
+			if (null != ing)
+				urls = ing.getUrls();
 
-		String regx = Strings.sNull(config.getInitParameter("ignore"), IGNORE);
-		if (!"null".equalsIgnoreCase(regx)) {
-			ignorePtn = Pattern.compile(regx, Pattern.CASE_INSENSITIVE);
+			String regx = Strings.sNull(config.getInitParameter("ignore"), IGNORE);
+			if (!"null".equalsIgnoreCase(regx)) {
+				ignorePtn = Pattern.compile(regx, Pattern.CASE_INSENSITIVE);
+			}
 		}
 	}
 
