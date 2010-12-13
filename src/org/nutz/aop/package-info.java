64751fa6217827ce4d46a6a06986ebe019ef5ae7
zozoh <zozohtnt@gmail.com>
2010-12-13 05:22:43 +0000

    修改 aop, castor, dao 的 package-info
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1710 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/aop/package-info.java
+++ src/org/nutz/aop/package-info.java
@@ -1,4 +1,7 @@
 /**
- * Nutz的Aop实现,通过内置的Asm生成指定类的子类,并插入Aop代码
+ * 提供对 Java 类的拦截能力
+ * <p>
+ * 通过  MeothodInterceptor 接口，对于 Java 类 public | protected 函数的提供了拦截能力。
+ * 具体的做法是为被拦截类生成子类，并通过 ASM 生成字节码
  */
 package org.nutz.aop;
