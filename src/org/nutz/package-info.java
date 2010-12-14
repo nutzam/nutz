023568080e4d4e96dc3e2e8733865f12f76a0768
zozoh <zozohtnt@gmail.com>
2010-12-14 16:46:23 +0000

    fixed issue 336
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1713 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/package-info.java
+++ src/org/nutz/package-info.java
@@ -1,4 +1,8 @@
 /**
- * 开源的，并且是完全免费的,同时也是商业友好的(Licensed under the Apache License, Version 2.0)。
+ * 对于 Java 程序员来说，除 SSH 之外，的另一个选择:
+ * <p>
+ * <b>Nutz</b>
+ * <p>
+ * 当然，它是开源的，并且是完全免费的,同时也是商业友好的(Licensed under the Apache License, Version 2.0)。
  */
 package org.nutz;
