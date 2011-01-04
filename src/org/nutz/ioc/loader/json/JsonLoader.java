283789414ec55185ffb8221de8b41889e29b0fca
wendal <wendal1985@gmail.com>
2011-01-04 14:19:38 +0000

    优化掉一个JsonLoader的一个方法
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1743 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/ioc/loader/json/JsonLoader.java
+++ src/org/nutz/ioc/loader/json/JsonLoader.java
@@ -1,8 +1,6 @@
 package org.nutz.ioc.loader.json;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
@@ -10,7 +8,6 @@ import java.util.Map;
 
 import org.nutz.ioc.loader.map.MapLoader;
 import org.nutz.json.Json;
-import org.nutz.lang.Encoding;
 import org.nutz.lang.Lang;
 import org.nutz.resource.NutResource;
 import org.nutz.resource.Scans;
@@ -35,22 +32,18 @@ public class JsonLoader extends MapLoader {
 		List<NutResource> list = Scans.me().loadResource("^(.+[.])(js|json)$", paths);
 		try {
 			for (NutResource nr : list)
-				loadFromInputStream(nr.getInputStream());
+				loadFromReader(nr.getReader());
 		}
 		catch (IOException e) {
 			throw Lang.wrapThrow(e);
 		}
 	}
 
-	private void loadFromInputStream(InputStream ins) {
-		loadFromReader(new InputStreamReader(ins, Encoding.CHARSET_UTF8));
-	}
-
 	private void loadFromReader(Reader reader) {
 		String s = Lang.readAll(reader);
 		Map<String, Map<String, Object>> map = (Map<String, Map<String, Object>>) Json.fromJson(s);
 		if (null != map && map.size() > 0)
-			this.getMap().putAll(map);
+			getMap().putAll(map);
 	}
 
 }
