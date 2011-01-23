10ba0a47b0c3405fd813ea64cc6dc32101e07ed1
wendal <wendal1985@gmail.com>
2011-01-23 09:12:39 +0000

    提交一个能够导致BoneCP+H2出错的测试用例.
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1774 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ test/org/nutz/dao/test/normal/BoneCP_Test.java
@@ -0,0 +1,49 @@
+package org.nutz.dao.test.normal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.nutz.dao.DaoException;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.TableName;
+import org.nutz.dao.sql.Sql;
+import org.nutz.dao.test.DaoCase;
+import org.nutz.dao.test.meta.Base;
+import org.nutz.dao.test.meta.Pet;
+import org.nutz.dao.test.meta.Platoon;
+import org.nutz.dao.test.meta.Tank;
+import org.nutz.dao.tools.Tables;
+import org.nutz.trans.Atom;
+
+
+public class BoneCP_Test extends DaoCase {
+	
+	private Platoon platoon;
+
+	@Override
+	protected void before() {
+		pojos.init();
+		platoon = pojos.create4Platoon(Base.make("blue"), "seals");
+	}
+	
+	@Test
+	public void clear_links() {
+		TableName.run(platoon, new Atom() {
+			public void run() {
+				Tank t = dao.fetchLinks(dao.fetch(Tank.class, "T92"), "members");
+				dao.clearLinks(t, "members");
+			}
+		});
+		Tables.define(dao, Tables.loadFrom("org/nutz/dao/test/meta/pet.dod"));
+		assertEquals(0, dao.count(Pet.class));
+		Sql sql1 = Sqls.create("INSERT INTO t_pet (name) VALUES ('A')");
+		Sql sql2 = Sqls.create("INSERT INTO t_pet (nocol) VALUES ('B')");
+		try {
+			dao.execute(sql1, sql2);
+			fail();
+		}
+		catch (DaoException e) {}
+		assertEquals(0, dao.count(Pet.class));
+	}
+}
