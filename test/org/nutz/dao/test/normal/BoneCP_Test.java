8dae9ef19e7df557f879c75ac674ed5474cd48a4
wendal <wendal1985@gmail.com>
2011-01-23 10:26:13 +0000

    再进一步净化BoneCP测试用例.
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1776 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/dao/test/normal/BoneCP_Test.java
+++ test/org/nutz/dao/test/normal/BoneCP_Test.java
@@ -6,32 +6,23 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.nutz.dao.DaoException;
 import org.nutz.dao.Sqls;
-import org.nutz.dao.TableName;
 import org.nutz.dao.sql.Sql;
 import org.nutz.dao.test.DaoCase;
 import org.nutz.dao.test.meta.Base;
 import org.nutz.dao.test.meta.Pet;
-import org.nutz.dao.test.meta.Platoon;
-import org.nutz.dao.test.meta.Tank;
 import org.nutz.dao.tools.Tables;
-import org.nutz.trans.Atom;
 
 
 public class BoneCP_Test extends DaoCase {
-	
-	private Platoon platoon;
 
 	@Override
 	protected void before() {
 		pojos.init();
-		platoon = pojos.create4Platoon(Base.make("blue"), "seals");
+		pojos.create4Platoon(Base.make("blue"), "seals");
 	}
 	
 	@Test
 	public void clear_links() {
-		TableName.run(platoon, new Atom() {
-			public void run() {}
-		});
 		Tables.define(dao, Tables.loadFrom("org/nutz/dao/test/meta/pet.dod"));
 		Sql sql1 = Sqls.create("INSERT INTO t_pet (name) VALUES ('A')");
 		Sql sql2 = Sqls.create("INSERT INTO t_pet (nocol) VALUES ('B')");
