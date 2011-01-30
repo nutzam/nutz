71366e683748c84d5a1dc78e4d6ec301295c85a4
wendal <wendal1985@gmail.com>
2011-01-30 08:22:12 +0000

    about Issue 371 ,轻微修正
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1799 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/adaptor/injector/ObjcetNaviNode.java
+++ src/org/nutz/mvc/adaptor/injector/ObjcetNaviNode.java
@@ -2,16 +2,20 @@ package org.nutz.mvc.adaptor.injector;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+
 import org.nutz.lang.Mirror;
 import org.nutz.lang.inject.Injecting;
 
 /**
  * request对象导航注入节点树
  * 
- * @author juqkai (juqkai@gmail.com) 2011-1-28
+ * @author zozoh(zozohtnt@gmail.com)
+ * @author juqkai (juqkai@gmail.com)
+ * @author wendal(wendal1985@gmail.com)
  */
 class ObjcetNaviNode {
-	private char separator = '.';
+	private static final char separator = '.';
 	private String name;
 	private Object value;
 	private boolean leaf = true;
@@ -20,7 +24,6 @@ class ObjcetNaviNode {
 	/**
 	 * 初始化当前结点
 	 * 
-	 * @author juqkai (juqkai@gmail.com)
 	 */
 	public void put(String path, Object value) {
 		name = fetchName(path);
@@ -29,13 +32,13 @@ class ObjcetNaviNode {
 			return;
 		}
 		leaf = false;
+		//TODO 如果最后一个字符为'.',会报错,是否应该处理?
 		addChild(path.substring(path.indexOf(separator) + 1), value);
 	}
 
 	/**
 	 * 添加子结点
 	 * 
-	 * @author juqkai (juqkai@gmail.com)
 	 */
 	private void addChild(String path, Object value) {
 		String subname = fetchName(path);
@@ -50,7 +53,6 @@ class ObjcetNaviNode {
 	/**
 	 * 取得节点名
 	 * 
-	 * @author juqkai (juqkai@gmail.com)
 	 */
 	private String fetchName(String path) {
 		if (path.indexOf(separator) <= 0) {
@@ -61,26 +63,28 @@ class ObjcetNaviNode {
 
 	/**
 	 * 将结点树中的值注入到 mirror 中
-	 * @param mirror 待注入对象
-	 * @return
+	 * 
+	 * @param mirror
+	 *            待注入对象
 	 */
 	public Object inject(Mirror<?> mirror) {
 		Object obj = mirror.born();
-		for (String key : child.keySet()) {
-			ObjcetNaviNode onn = child.get(key);
-				Injecting in = mirror.getInjecting(key);
-				if (onn.isLeaf()) {
-					in.inject(obj, onn.getValue());
-					continue;
-				}
-				// 不是叶子结点,不能直接注入
-				Mirror<?> fieldMirror;
-				try {
-					fieldMirror = Mirror.me(mirror.getField(key).getType());
-					in.inject(obj, onn.inject(fieldMirror));
-				} catch (NoSuchFieldException e) {
-					continue;
-				}
+		for (Entry<String, ObjcetNaviNode> entry : child.entrySet()) {
+			ObjcetNaviNode onn = entry.getValue();
+			Injecting in = mirror.getInjecting(entry.getKey());
+			if (onn.isLeaf()) {
+				in.inject(obj, onn.getValue());
+				continue;
+			}
+			// 不是叶子结点,不能直接注入
+			Mirror<?> fieldMirror;
+			try {
+				fieldMirror = Mirror.me(mirror.getField(entry.getKey()).getType());
+				in.inject(obj, onn.inject(fieldMirror));
+			}
+			catch (NoSuchFieldException e) {
+				continue;//TODO 是不是应该log一下呢?
+			}
 		}
 		return obj;
 	}
--- src/org/nutz/mvc/adaptor/injector/ObjectNavlPairInjector.java
+++ src/org/nutz/mvc/adaptor/injector/ObjectNavlPairInjector.java
@@ -9,31 +9,35 @@ import org.nutz.lang.Strings;
 import org.nutz.mvc.adaptor.ParamInjector;
 
 /**
- * 对象导航注入器
- * 默认情况下只有使用 @Param("::") 的情况下才调用这个注入器,
+ * 对象导航注入器 默认情况下只有使用 @Param("::") 的情况下才调用这个注入器
+ * <p/>
  * 毕竟它在接收到请求时进行注入,会有一定的性能损伤
+ * 
  * @author juqkai(juqkai@gmail.com)
- *
+ * @author wendal(wendal1985@gmail.com)
+ * 
  */
-public class ObjectNavlPairInjector implements ParamInjector{
+public class ObjectNavlPairInjector implements ParamInjector {
 	protected Mirror<?> mirror;
 	private String prefix;
-	
+
 	public ObjectNavlPairInjector(String prefix, Class<?> type) {
 		prefix = Strings.isBlank(prefix) ? "" : Strings.trim(prefix);
 		this.prefix = prefix;
 		this.mirror = Mirror.me(type);
 	}
-	
-	public Object get(ServletContext sc, HttpServletRequest req, HttpServletResponse resp, Object refer) {
+
+	public Object get(	ServletContext sc,
+						HttpServletRequest req,
+						HttpServletResponse resp,
+						Object refer) {
 		ObjcetNaviNode no = new ObjcetNaviNode();
 		String pre = "";
-		if(prefix.equals("")){
+		if ("".equals(prefix))
 			pre = "node.";
-		}
-		for(Object name : req.getParameterMap().keySet()){
+		for (Object name : req.getParameterMap().keySet()) {
 			String na = (String) name;
-			if(na.startsWith(prefix)){
+			if (na.startsWith(prefix)) {
 				no.put(pre + na, req.getParameter(na));
 			}
 		}
