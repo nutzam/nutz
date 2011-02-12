d83f180b5a7bf8fe0df6f4e1cce46c370befcc5a
wendal <wendal1985@gmail.com>
2011-02-12 12:20:30 +0000

    为发布1.b.35 先回滚mvc2和mvc module
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1828 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc2/annotation/Chain.java
+++
@@ -1,8 +0,0 @@
-package org.nutz.mvc2.annotation;
-
-public @interface Chain {
-
-	String factory() default "";
-	
-	String[] args() default {};
-}
--- src/org/nutz/mvc2/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 基于动作链的MVC版本
- */
-package org.nutz.mvc2;
