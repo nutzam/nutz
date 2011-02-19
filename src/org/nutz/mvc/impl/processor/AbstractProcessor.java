29e3698466d4c4b684312139c03b4ad44111dcad
wendal <wendal1985@gmail.com>
2011-02-19 14:48:17 +0000

    按照我的理解,对新重构完的MVC做轻微修改-->修改AbstractProcessor,添加继续动作链的静态方法
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1861 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/impl/processor/AbstractProcessor.java
+++ src/org/nutz/mvc/impl/processor/AbstractProcessor.java
@@ -5,7 +5,7 @@ import org.nutz.mvc.impl.Processor;
 
 public abstract class AbstractProcessor implements Processor {
 
-	private Processor next;
+	protected Processor next;
 
 	public void setNext(Processor next) {
 		this.next = next;
@@ -17,9 +17,30 @@ public abstract class AbstractProcessor implements Processor {
 
 	public Processor process(ActionContext ac) throws Throwable {
 		doProcess(ac);
-		return next;
+		return getNext();
+	}
+	
+	/**
+	 * 继续执行其他处理器
+	 * <p/><b>如果调用本方法,process方法务必返回null,否则动作链将再次被执行</b>
+	 * <p/><b>如果调用本方法,process方法务必返回null,否则动作链将再次被执行</b>
+	 * <p/><b>不应该在doProcess方法中调用本方法</b>
+	 * <p/>调用示例:<p/>
+	 * <code>
+	 * logger.info("已经授权,继续执行");<p/>
+	 * doChain(ac, getNext());<p/>
+	 * logger.info("处理完成,将请求记录到数据库");
+	 * </code>
+	 */
+	protected static final void doChain(ActionContext ac, Processor p) throws Throwable {
+		if (null != p)//还有其他处理器?那继续吧
+			while (null != (p = p.process(ac))) {}
 	}
 
-	public abstract void doProcess(ActionContext ac) throws Throwable;
+	/**
+	 * 一般都应该覆盖这个方法,默认实现不做任何事情
+	 */
+	public void doProcess(ActionContext ac) throws Throwable {
+	}
 
 }
