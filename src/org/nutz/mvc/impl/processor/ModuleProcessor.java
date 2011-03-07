1e8def234187a337685971c6ea97917244f53630
wendal <wendal1985@gmail.com>
2011-03-07 12:19:36 +0000

    修正MVC重构导致ioc的scope为request的bean失效的bug.
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1895 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/impl/processor/ModuleProcessor.java
+++ src/org/nutz/mvc/impl/processor/ModuleProcessor.java
@@ -2,10 +2,17 @@ package org.nutz.mvc.impl.processor;
 
 import java.lang.reflect.Method;
 
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.Ioc2;
+import org.nutz.ioc.IocContext;
+import org.nutz.ioc.impl.ComboContext;
+import org.nutz.lang.Lang;
 import org.nutz.lang.Strings;
 import org.nutz.mvc.ActionContext;
 import org.nutz.mvc.ActionInfo;
 import org.nutz.mvc.NutConfig;
+import org.nutz.mvc.ioc.RequestIocContext;
+import org.nutz.mvc.ioc.SessionIocContext;
 
 /**
  * 
@@ -14,8 +21,6 @@ import org.nutz.mvc.NutConfig;
  *
  */
 public class ModuleProcessor extends AbstractProcessor {
-	
-	private Object module;
 	
 	private String injectName;
 	
@@ -28,14 +33,47 @@ public class ModuleProcessor extends AbstractProcessor {
 		if (!Strings.isBlank(ai.getInjectName())) {
 			moduleType = ai.getModuleType();
 			injectName = ai.getInjectName();
-		} else
-			module = ai.getModuleType().newInstance();
+		}
 	}
 
 	public void process(ActionContext ac) throws Throwable {
-		ac.setModule(module == null ? ac.getIoc().get(moduleType, injectName) : module);
-		ac.setMethod(method);
-		doNext(ac);
+		RequestIocContext reqContext = null;
+		try {
+			if (Strings.isBlank(injectName)) {
+				ac.setModule(moduleType.newInstance());
+			} else {
+				Ioc ioc = ac.getIoc();
+				if (null == ioc)
+					throw Lang.makeThrow(	"Moudle with @InjectName('%s') but you not declare a Ioc for this app",
+							injectName);
+				Object obj;
+				/*
+				 * 如果 Ioc 容器实现了高级接口，那么会为当前请求设置上下文对象
+				 */
+				if (ioc instanceof Ioc2) {
+					reqContext = new RequestIocContext(ac.getRequest());
+					SessionIocContext sessionContext = new SessionIocContext(ac.getRequest().getSession());
+					IocContext myContext = new ComboContext(reqContext, sessionContext);
+					obj = ((Ioc2) ioc).get(moduleType, injectName, myContext);
+				}
+				/*
+				 * 否则，则仅仅简单的从容器获取
+				 */
+				else
+					obj = ioc.get(moduleType, injectName);
+				ac.setModule(obj);
+
+			}
+			ac.setMethod(method);
+			doNext(ac);
+		} finally {
+			if (reqContext != null)
+				try {
+					reqContext.depose();
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+		}
 	}
 	
 }
