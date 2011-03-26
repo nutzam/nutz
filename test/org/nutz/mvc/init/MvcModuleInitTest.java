959406bd4246c4d7dd8e0d21f34c6664af5eb40a
zozoh <zozohtnt@gmail.com>
2011-03-26 17:30:35 +0000

    重现了 Issue 417
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1934 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ test/org/nutz/mvc/init/MvcModuleInitTest.java
@@ -0,0 +1,34 @@
+package org.nutz.mvc.init;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.nutz.mvc.AbstractMvcTest;
+import org.nutz.mvc.init.conf.MainModuleA;
+
+public class MvcModuleInitTest extends AbstractMvcTest {
+
+	protected void initServletConfig() {
+		servletConfig.addInitParameter("modules", MainModuleA.class.getName());
+	}
+
+	@Test
+	public void test_array_long_param() throws Exception {
+		request.setPathInfo("/param/a");
+		request.setParameter("ids", "1,2,3");
+		servlet.service(request, response);
+		String re = response.getAsString();
+		assertEquals("[1, 2, 3]", re);
+	}
+	
+	@Test
+	public void test_array_long_param2() throws Exception {
+		request.setPathInfo("/param/b");
+		request.setParameter("nm", "xyz");
+		request.setParameter("ids", "1,2,3");
+		servlet.service(request, response);
+		String re = response.getAsString();
+		assertEquals("xyz[1, 2, 3]", re);
+	}
+
+}
--- test/org/nutz/mvc/init/conf/MainModuleA.java
+++ test/org/nutz/mvc/init/conf/MainModuleA.java
@@ -1,16 +1,27 @@
 package org.nutz.mvc.init.conf;
 
+import java.util.List;
+
 import org.nutz.json.Json;
+import org.nutz.lang.Lang;
 import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.Ok;
 import org.nutz.mvc.annotation.Param;
 
+@Fail("json")
 public class MainModuleA {
 
 	@At("/param/a")
+	@Ok("json")
+	public List<String> f_A(@Param("nm") String name, @Param("ids") long[] ids) {
+		return Lang.list(Json.toJson(ids));
+	}
+
+	@At("/param/b")
 	@Ok("raw")
-	public String paramIsArray(@Param("ids") long[] ids) {
-		return Json.toJson(ids);
+	public String f_B(@Param("nm") String name, @Param("ids") long[] ids) {
+		return name + Json.toJson(ids);
 	}
 
 }
