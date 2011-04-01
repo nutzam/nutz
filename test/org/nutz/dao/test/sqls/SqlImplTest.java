c055b315badc9fbdaccc3461ec5da1083aa0ee33
wendal <wendal1985@gmail.com>
2011-04-01 01:31:58 +0000

    Fix issue 425
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1951 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/dao/test/sqls/SqlImplTest.java
+++ test/org/nutz/dao/test/sqls/SqlImplTest.java
@@ -22,7 +22,7 @@ public class SqlImplTest {
 
 		sql.getContext().setResult(list);
 
-		List<?> re = sql.getList(Map.class);// 传人 map结果会导致上面的isFrom 为false
+		List<?> re = sql.getList(Map.class);// 传入 map结果会导致上面的isFrom 为false
 		assertTrue(re == list);
 
 		re = sql.getList(HashMap.class);// 因为list中的实例是HashMap,因此能够正常返回
