2222b99f0234f8e3fde36fab49d84365c6827de8
wendal <wendal1985@gmail.com>
2011-04-13 09:10:18 +0000

    ...
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1976 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- test/org/nutz/mvc/testapp/AllApp.java
+++ test/org/nutz/mvc/testapp/AllApp.java
@@ -2,7 +2,9 @@ package org.nutz.mvc.testapp;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.nutz.mvc.testapp.views.AllView;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({BaseTest.class})
+@Suite.SuiteClasses({BaseTest.class,
+					 AllView.class})
 public class AllApp {}
---
+++ test/org/nutz/mvc/testapp/views/AllView.java
@@ -0,0 +1,13 @@
+package org.nutz.mvc.testapp.views;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ForwardViewTest.class,
+			   JspViewTest.class,
+			   ServerRedirectViewTest.class})
+public class AllView {
+
+}
