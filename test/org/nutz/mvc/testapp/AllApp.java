0870040282ba657f370a16e5e0860869130bde15
zozoh <zozohtnt@gmail.com>
2011-04-13 08:44:09 +0000

    忘了传了...
    git-svn-id: http://nutz.googlecode.com/svn/trunk@1975 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

---
+++ test/org/nutz/mvc/testapp/AllApp.java
@@ -0,0 +1,8 @@
+package org.nutz.mvc.testapp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BaseTest.class})
+public class AllApp {}
