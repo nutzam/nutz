aa374d5e0d9463daec3a9d06dd933a70ca2d9c66
zozoh <zozohtnt@gmail.com>
2011-05-04 06:02:02 +0000

    接 r2021 ，传少了的这个文件
    git-svn-id: http://nutz.googlecode.com/svn/trunk@2027 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/lang/util/AbstractContext.java
+++ src/org/nutz/lang/util/AbstractContext.java
@@ -1,13 +1,20 @@
 package org.nutz.lang.util;
 
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.nutz.castor.Castors;
+import org.nutz.lang.Mirror;
 
-//TODO XXX 这是临时伪造的,等待zozoh上传真实版本
 public abstract class AbstractContext implements Context {
-	
+
+	public AbstractContext() {
+		super();
+	}
+
 	public <T> T getAs(Class<T> type, String name) {
 		return Castors.me().castTo(get(name), type);
 	}
@@ -28,6 +35,47 @@ public abstract class AbstractContext implements Context {
 		return getAs(float.class, name);
 	}
 
+	public Context putAll(Object obj) {
+		if (null != obj) {
+			// Context
+			if (obj instanceof Context) {
+				for (String key : ((Context) obj).keys())
+					this.set(key, ((Context) obj).get(key));
+			}
+			// Map
+			else if (obj instanceof Map<?, ?>) {
+				for (Map.Entry<?, ?> en : ((Map<?, ?>) obj).entrySet())
+					this.set(en.getKey().toString(), en.getValue());
+			}
+			// 普通 Java 对象
+			else {
+				Mirror<?> mirror = Mirror.me(obj);
+				// 需要被忽略的 Java 对象
+				if (mirror.getType().isArray()
+					|| mirror.isNumber()
+					|| mirror.isBoolean()
+					|| mirror.isChar()
+					|| mirror.isStringLike()
+					|| mirror.isDateTimeLike()
+					|| Collection.class.isAssignableFrom(mirror.getType())) {}
+				// 普通 Java 对象，应该取其每个字段
+				else {
+					for (Field field : mirror.getFields()) {
+						this.set(field.getName(), mirror.getValue(obj, field));
+					}
+				}
+			}
+		}
+		return this;
+	}
+
+	public Map<String, Object> getInnerMap() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		for (String key : this.keys())
+			map.put(key, this.get(key));
+		return map;
+	}
+
 	@SuppressWarnings("unchecked")
 	public Map<String, Object> getMap(String name) {
 		return getAs(Map.class, name);
@@ -38,20 +86,6 @@ public abstract class AbstractContext implements Context {
 		return getAs(List.class, name);
 	}
 
-	public Context clone() {
-		SimpleContext context = new SimpleContext();
-		for (String key : keys()) {
-			context.set(key, get(key));
-		}
-		return context;
-	}
-	
-	public Map<String, Object> getInnerMap() {
-		return null;
-	}
-	
-	
-	public Context putAll(Object obj) {
-		return this;
-	}
+	public abstract AbstractContext clone();
+
 }
