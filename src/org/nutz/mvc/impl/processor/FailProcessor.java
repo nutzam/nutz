72de8eafb250ea870442c08f33103197e585f5e0
wendal <wendal1985@gmail.com>
2011-05-12 03:46:50 +0000

    将MVC发生错误时日志输出级别调高，方便用户查错
    git-svn-id: http://nutz.googlecode.com/svn/trunk@2065 423f10f2-e3a4-11dd-a6ea-a32d6b26a33d

--- src/org/nutz/mvc/impl/processor/FailProcessor.java
+++ src/org/nutz/mvc/impl/processor/FailProcessor.java
@@ -23,8 +23,8 @@ public class FailProcessor extends ViewProcessor {
 	
 	@Override
 	public void process(ActionContext ac) throws Throwable {
-		if (log.isDebugEnabled())
-			log.debug("Catch handle error", ac.getError());
+		if (log.isErrorEnabled())
+			log.error("Catch handle error", ac.getError());
 		super.process(ac);
 	}
 }
