c141f9d0911f28334a05a7de4e3ac0e7f4d73c4a
juqkai <juqkai@gmail.com>
2011-06-16 00:45:56 +0800

    添加预编译的尝试性实现

---
+++ src/org/nutz/el2/El2pre.java
@@ -0,0 +1,31 @@
+package org.nutz.el2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.nutz.el2.arithmetic.RPNCalculate;
+import org.nutz.el2.arithmetic.ShuntingYard;
+import org.nutz.lang.util.Context;
+
+/**
+ * 预编译
+ * @author juqkai(juqkai@gmail.com)
+ *
+ */
+public class El2pre {
+	private ShuntingYard sy = new ShuntingYard();
+	private RPNCalculate rc = new RPNCalculate();
+	private Queue<Object> rpn;
+	
+	public El2pre(String val) {
+		rpn = sy.parseToRPN(val);
+	}
+
+	public Object eval(Context context) {
+		Queue<Object> obj = new LinkedList<Object>();
+		obj.addAll(rpn);
+		return rc.calculate(context, obj);
+	}
+	
+	//@ JKTODO 添加自动找包,比如现在就不能使用Boolean.FALSE来得到false
+}
