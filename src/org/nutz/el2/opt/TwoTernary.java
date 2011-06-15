611ebc9fac1555a44be8073fd8764c0890469afd
juqkai <juqkai@gmail.com>
2011-06-15 11:02:40 +0800

    调整方法调用的实现机制,以使其更符合逆波兰表达的的概念

--- src/org/nutz/el2/opt/TwoTernary.java
+++ src/org/nutz/el2/opt/TwoTernary.java
@@ -2,6 +2,8 @@ package org.nutz.el2.opt;
 
 import java.util.Queue;
 
+import org.nutz.el2.Operator;
+
 /**
  * 二元运算,只是提取了公共部分
  * @author juqkai(juqkai@gmail.com)
@@ -14,5 +16,19 @@ public abstract class TwoTernary extends AbstractOpt{
 	public void wrap(Queue<Object> rpn){
 		right = rpn.poll();
 		left = rpn.poll();
+	}
+
+	public Object getRight() {
+		if(right instanceof Operator){
+			return ((Operator) right).calculate();
+		}
+		return right;
+	}
+
+	public Object getLeft() {
+		if(left instanceof Operator){
+			return ((Operator) left).calculate();
+		}
+		return left;
 	}
 }
--- src/org/nutz/el2/opt/object/InvokeMethodOpt.java
+++ src/org/nutz/el2/opt/object/InvokeMethodOpt.java
@@ -1,12 +1,8 @@
 package org.nutz.el2.opt.object;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 
-import org.nutz.el2.obj.IdentifierObj;
 import org.nutz.el2.opt.AbstractOpt;
-import org.nutz.lang.Mirror;
 
 /**
  * 执行
@@ -15,42 +11,24 @@ import org.nutz.lang.Mirror;
  */
 public class InvokeMethodOpt extends AbstractOpt {
 	private Object left;
-	private List<Object> right;
 
 	public int fetchPriority() {
-		return 0;
-	}
-
-	public void wrap(Queue<Object> operand) {
-		right = new ArrayList<Object>();
-		//读取参数列表
-		while(!operand.isEmpty() && !(operand.peek() instanceof ListOpt)){
-			right.add(0, operand.poll());
-		}
-		if(operand.peek() instanceof ListOpt){
-			operand.poll();
-		}
-		
-		left = operand.poll();
+		return 1;
 	}
 
 	public Object calculate() {
-		if(!(left instanceof AccessOpt)){
-			//@ JKTODO 添加自定义方法的调用
-			return null;
+		if(left instanceof ListOpt){
+			return ((ListOpt) left).calculate();
 		}
-		AccessOpt lval = (AccessOpt) left;
-		Object[] objs = (Object[]) lval.calculate();
-		Object obj = objs[0];
-		if(objs[0] instanceof IdentifierObj){
-			obj = ((IdentifierObj) objs[0]).fetchVal();
-		}
-		Object method = objs[1];
-		return Mirror.me(obj).invoke(obj, method.toString(), right.toArray());
+		return null;
 	}
 
 	public String fetchSelf() {
 		return "invoke";
+	}
+
+	public void wrap(Queue<Object> operand) {
+		left = operand.poll();
 	}
 
 }
