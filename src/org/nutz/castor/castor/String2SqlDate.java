459ff92a60ad12c32895b605166265e6e3a0622f
wendal <wendal1985@gmail.com>
2011-09-06 14:40:34 +0800

    fix http://code.google.com/p/nutz/issues/detail?id=423

--- src/org/nutz/castor/castor/String2SqlDate.java
+++ src/org/nutz/castor/castor/String2SqlDate.java
@@ -4,11 +4,14 @@ import java.text.DateFormat;
 import java.text.ParseException;
 
 import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
 
 public class String2SqlDate extends DateTimeCastor<String, java.sql.Date> {
 
 	@Override
 	public java.sql.Date cast(String src, Class<?> toType, String... args) {
+		if (Strings.isBlank(src))
+			return null;
 		try {
 			return new java.sql.Date(((DateFormat) dateFormat.clone()).parse(src).getTime());
 		}
--- src/org/nutz/castor/castor/String2SqlTime.java
+++ src/org/nutz/castor/castor/String2SqlTime.java
@@ -4,11 +4,14 @@ import java.text.DateFormat;
 import java.text.ParseException;
 
 import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
 
 public class String2SqlTime extends DateTimeCastor<String, java.sql.Time> {
 
 	@Override
 	public java.sql.Time cast(String src, Class<?> toType, String... args) {
+		if (Strings.isBlank(src))
+			return null;
 		try {
 			return new java.sql.Time(((DateFormat) timeFormat.clone()).parse(src).getTime());
 		}
