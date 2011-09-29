da7d49b9e97e15711c5a71ee84d5a309b7166f62
Peter Zhang <zozohtnt@gmail.com>
2011-09-30 03:15:31 +0800

    丰富文件池的方法

---
+++ src/org/nutz/lang/ContinueLoop.java
@@ -0,0 +1,9 @@
+package org.nutz.lang;
+
+/**
+ * 继续循环，如果正在递归，则停止继续递归
+ * 
+ * @author zozoh(zozohtnt@gmail.com)
+ */
+@SuppressWarnings("serial")
+public class ContinueLoop extends RuntimeException {}
