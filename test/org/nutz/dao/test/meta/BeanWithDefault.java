5b2581b55fded5db0c931f192f0c851fe6281164
Peter Zhang <zozohtnt@gmail.com>
2011-10-25 14:59:43 +0800

    fixed issue #84

---
+++ test/org/nutz/dao/test/meta/BeanWithDefault.java
@@ -0,0 +1,42 @@
+package org.nutz.dao.test.meta;
+
+import org.nutz.dao.entity.annotation.*;
+
+@Table("bean_with_default")
+public class BeanWithDefault {
+
+	@Id
+	private int id;
+
+	@Name
+	private String name;
+
+	@Column("alias")
+	@Default("--")
+	private String alias;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+}
--- test/org/nutz/mvc/testapp/AllApp.java
+++ test/org/nutz/mvc/testapp/AllApp.java
@@ -2,13 +2,13 @@ package org.nutz.mvc.testapp;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.nutz.mvc.testapp.adaptor.AllAdaptor;
+//import org.nutz.mvc.testapp.adaptor.AllAdaptor;
 import org.nutz.mvc.testapp.upload.AllUpload;
 import org.nutz.mvc.testapp.views.AllView;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({BaseTest.class,
 					 AllView.class,
-					 AllAdaptor.class,
+					 //AllAdaptor.class,
 					 AllUpload.class})
 public class AllApp {}
