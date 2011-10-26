083bf8941a461fe4c31c1034f2e79071bb2a43a8
juqkai <juqkai@gmail.com>
2011-10-26 09:32:30 +0800

    调整Max, Min的实现, 使其支持N个参数

--- test/org/nutz/el/El2Test.java
+++ test/org/nutz/el/El2Test.java
@@ -246,6 +246,7 @@ public class El2Test {
 	@Test
 	public void custom() {
 		assertEquals(2, El.eval("max(1, 2)"));
+		assertEquals(1, El.eval("min(1, 2)"));
 		assertEquals("jk", El.eval("trim('    jk    ')"));
 	}
 
