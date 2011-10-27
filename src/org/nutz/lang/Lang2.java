021982c5a2175e511f058334f149157e603bdf52
juqkai <juqkai@gmail.com>
2011-10-27 17:06:51 +0800

    add inject List and Map

--- src/org/nutz/lang/Lang2.java
+++ src/org/nutz/lang/Lang2.java
@@ -2,10 +2,10 @@ package org.nutz.lang;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.nutz.castor.Castors;
 import org.nutz.lang.inject.Injecting;
@@ -13,29 +13,58 @@ import org.nutz.lang.inject.Injecting;
 public class Lang2 {
     public static Object inject(Object model, Type type){
         Mirror<?> me = Mirror.me(type);
-//        if(me.is(List.class)){
-//            return injectList(me);
-//        } else if(me.is(Map.class)){
-//            return injectMap(me);
+        if(me.is(List.class)){
+            return injectList(model, me);
+        } else if(me.is(Map.class)){
+            return injectMap(model, me);
 //        } else if(me.is(Set.class)){
 //            return injectSet(me);
 //        } else if(me.getType().isArray()){
 //            return injectArray(me);
-//        }
+        }
         return injectObj(model, me);
     }
     
-    @SuppressWarnings({ "unused", "rawtypes" })
-    private static Object injectList(Object model, Mirror<?> me){
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static Object injectMap(Object model, Mirror<?> me){
         Map re = null;
         if(me.isInterface()){
             re = new HashMap();
         } else {
             re = (Map) me.born();
         }
-        return null;
+        
+        Map map = (Map) model;
+        for(Object key : map.keySet()){
+            Object val = map.get(key);
+            if(isLeaf(val)){
+                re.put(key, Castors.me().castTo(val, (Class<?>) me.getGenericsType(1)));
+            }
+            re.put(key, inject(val, me.getGenericsType(1)));
+        }
+        return re;
     }
     
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private static Object injectList(Object model, Mirror<?> me){
+        List re = null;
+        if(me.isInterface()){
+            re = new ArrayList();
+        } else {
+            re = (List) me.born();
+        }
+        
+        for(Object obj : (List) model){
+            if(isLeaf(obj)){
+                re.add(Castors.me().castTo(obj, (Class<?>) me.getGenericsType(0)));
+                continue;
+            }
+            re.add(inject(obj, me.getGenericsType(0)));
+        }
+        return re;
+    }
+    
+    @SuppressWarnings("unchecked")
     private static Object injectObj(Object model, Mirror<?> me) {
         Object obj = me.born();
         Map<String, ?> map = (Map<String, ?>) model;
@@ -44,7 +73,7 @@ public class Lang2 {
                 Field field = me.getField(key);
                 Object val = map.get(key);
                 Injecting in = me.getInjecting(key);
-                if(!(val instanceof Map || val instanceof List)){
+                if(isLeaf(val)){
                     in.inject(obj, Castors.me().castTo(val, field.getType()));
                     continue;
                 }
@@ -56,4 +85,13 @@ public class Lang2 {
         return obj;
     }
     
+    private static boolean isLeaf(Object obj){
+        if(obj instanceof Map){
+            return false;
+        }
+        if(obj instanceof List){
+            return false;
+        }
+        return true;
+    }
 }
