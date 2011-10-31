759213295d6e380f80e6701e20b7b237a81559fa
pangwu86 <pangwu86@gmail.com>
2011-10-31 11:49:27 +0800

    Add an new annotation ---- Comment for Create Table

---
+++ src/org/nutz/dao/entity/annotation/Comment.java
@@ -0,0 +1,19 @@
+package org.nutz.dao.entity.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 可以为表或字段添加相应的注释。
+ * 
+ * <b>动态表名暂时不支持</b>
+ * 
+ * @author pangwu86(pangwu86@gmail.com)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface Comment {
+	String value() default "";
+}
