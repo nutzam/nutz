5871bee93af85fe8b4bc9070d5c50c9115d4bd77
Peter Zhang <zozohtnt@gmail.com>
2012-01-25 22:06:17 +0800

    ...

---
+++ src/org/nutz/lang/util/Callback3.java
@@ -0,0 +1,15 @@
+package org.nutz.lang.util;
+
+/**
+ * 带三个参数的通用回调接口
+ * 
+ * @author zozoh(zozohtnt@gmail.com)
+ * @param <T0>
+ * @param <T1>
+ * @param <T2>
+ */
+public interface Callback3<T0, T1, T2> {
+
+	void invoke(T0 arg0, T1 arg1, T2 arg2);
+
+}
