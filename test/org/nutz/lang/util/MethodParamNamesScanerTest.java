6de01c55e64355ce2e1aab6f1622f1e60ebc12c1
wendal <wendal1985@gmail.com>
2012-03-10 23:14:22 +0800

    添加一个工具类:  通过读取Class文件,获得方法形参名称列表

---
+++ test/org/nutz/lang/util/MethodParamNamesScanerTest.java
@@ -0,0 +1,18 @@
+package org.nutz.lang.util;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.nutz.resource.Scans;
+
+public class MethodParamNamesScanerTest {
+
+	@Test
+	public void testGetParamNames() throws Throwable {
+		List<Class<?>> list = Scans.me().scanPackage("org.nutz");
+		for (Class<?> klass : list) {
+			MethodParamNamesScaner.getParamNames(klass) ;
+		}
+	}
+
+}
