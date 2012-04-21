5aeb1241a1a141725e023afbacaa06e9abc2a18d
feiyan35488 <feiyan35488@gmail.com>
2012-04-21 12:29:35 +0800

    complete Loadings and AnnotationLoader

---
+++ src/org/nutz/ioc/loader/annotation/AnnotationIocLoader2.java
@@ -0,0 +1,218 @@
+package org.nutz.ioc.loader.annotation;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.nutz.castor.Castors;
+import org.nutz.ioc.IocException;
+import org.nutz.ioc.IocLoader;
+import org.nutz.ioc.IocLoading;
+import org.nutz.ioc.ObjectLoadException;
+import org.nutz.ioc.annotation.InjectName;
+import org.nutz.ioc.meta.IocEventSet;
+import org.nutz.ioc.meta.IocField;
+import org.nutz.ioc.meta.IocObject;
+import org.nutz.ioc.meta.IocValue;
+import org.nutz.lang.Lang;
+import org.nutz.lang.Mirror;
+import org.nutz.lang.Strings;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+import org.nutz.resource.Scans;
+
+/**
+ * 基于注解的Ioc配置
+ * @author wendal(wendal1985@gmail.com)
+ * 
+ */
+public class AnnotationIocLoader2 implements IocLoader {
+
+	private static final Log LOG = Logs.get();
+
+	private HashMap<String, IocObject> map = new HashMap<String, IocObject>();
+
+	public AnnotationIocLoader2(String... packages) {
+		for (String packageZ : packages)
+			for (Class<?> classZ : Scans.me().scanPackage(packageZ))
+				addClass(classZ);
+		if (LOG.isInfoEnabled())
+			LOG.infof(	"Scan complete ! Found %s classes in %s base-packages!\nbeans = %s",
+						map.size(),
+						packages.length,
+						Castors.me().castToString(map.keySet()));
+	}
+
+	private void addClass(Class<?> classZ) {
+		if (classZ.isInterface()
+			|| classZ.isMemberClass()
+			|| classZ.isEnum()
+			|| classZ.isAnnotation()
+			|| classZ.isAnonymousClass())
+			return;
+		int modify = classZ.getModifiers();
+		if (Modifier.isAbstract(modify) || (!Modifier.isPublic(modify)))
+			return;
+		IocBean iocBean = classZ.getAnnotation(IocBean.class);
+		if (iocBean != null) {
+			if (LOG.isDebugEnabled())
+				LOG.debugf("Found a Class with Ioc-Annotation : %s", classZ);
+		}
+		String beanName = null;
+		if(iocBean != null){
+			// 采用 @IocBean->name
+			beanName = iocBean.name();
+			if (Strings.isBlank(beanName)) {
+				// 否则采用 @InjectName
+				InjectName innm = classZ.getAnnotation(InjectName.class);
+				if (null != innm && !Strings.isBlank(innm.value())) {
+					beanName = innm.value();
+				}
+				// 大哥（姐），您都不设啊!? 那就用 simpleName 吧
+				else {
+					beanName = Strings.lowerFirst(classZ.getSimpleName());
+				}
+			}
+		}else{
+			beanName = classZ.getName();
+		}
+		IocObject iocObject = new IocObject();
+		iocObject.setType(classZ);
+		map.put(beanName, iocObject);
+
+		if(iocBean != null){
+			iocObject.setSingleton(iocBean.singleton());
+			if (!Strings.isBlank(iocBean.scope()))
+				iocObject.setScope(iocBean.scope());
+		}else{
+			iocObject.setSingleton(true);
+			iocObject.setScope("app");
+		}
+		
+		if(iocBean != null){
+			// 看看构造函数都需要什么函数
+			String[] args = iocBean.args();
+			if (null != args && args.length > 0)
+				for (String value : args)
+					iocObject.addArg(convert(value));
+		}
+		if(iocBean != null){
+			// 设置Events
+			IocEventSet eventSet = new IocEventSet();
+			iocObject.setEvents(eventSet);
+			if (!Strings.isBlank(iocBean.create()))
+				eventSet.setCreate(iocBean.create().trim().intern());
+			if (!Strings.isBlank(iocBean.depose()))
+				eventSet.setDepose(iocBean.depose().trim().intern());
+			if (!Strings.isBlank(iocBean.fetch()))
+				eventSet.setFetch(iocBean.fetch().trim().intern());
+		}
+		// 处理字段(以@Inject方式,位于字段)
+		List<String> fieldList = new ArrayList<String>();
+		Mirror<?> mirror = Mirror.me(classZ);
+		Field[] fields = mirror.getFields(Inject.class);
+		for (Field field : fields) {
+			Inject inject = field.getAnnotation(Inject.class);
+			//无需检查,因为字段名是唯一的
+			//if(fieldList.contains(field.getName()))
+			//	throw duplicateField(classZ,field.getName());
+			IocField iocField = new IocField();
+			iocField.setName(field.getName());
+			IocValue iocValue;
+			if (Strings.isBlank(inject.value())) {
+				iocValue = new IocValue();
+				iocValue.setType(IocValue.TYPE_REFER);
+				iocValue.setValue(field.getName());
+			} else
+				iocValue = convert(inject.value());
+			iocField.setValue(iocValue);
+			iocObject.addField(iocField);
+			fieldList.add(iocField.getName());
+		}
+		// 处理字段(以@Inject方式,位于set方法)
+		Method[] methods = mirror.getMethods();
+		for (Method method : methods) {
+			Inject inject = method.getAnnotation(Inject.class);
+			
+			//过滤特殊方法
+			int m = method.getModifiers();
+			if(Modifier.isAbstract(m) || (!Modifier.isPublic(m))
+					|| Modifier.isStatic(m))
+				continue;
+			if (method.getName().startsWith("set") 
+				&& method.getName().length() > 3
+				&& method.getParameterTypes().length == 1) {
+				IocField iocField = new IocField();
+				iocField.setName(Strings.lowerFirst(method.getName().substring(3)));
+				if(fieldList.contains(iocField.getName()))
+					throw duplicateField(classZ,iocField.getName());
+				IocValue iocValue;
+				if (inject == null || Strings.isBlank(inject.value())) {
+					iocValue = new IocValue();
+					iocValue.setType(IocValue.TYPE_REFER);
+					iocValue.setValue(Strings.lowerFirst(method.getName().substring(3)));
+				} else
+					iocValue = convert(inject.value());
+				iocField.setValue(iocValue);
+				iocObject.addField(iocField);
+				fieldList.add(iocField.getName());
+			}
+		}
+		if(iocBean != null){
+			// 处理字段(以@IocBean.field方式),只允许引用同名的bean, 就映射为 refer:FieldName
+			String[] flds = iocBean.fields();
+//			if (null == flds || flds.length == 0) {
+//				flds = iocBean.field();
+//			}
+			if (flds != null && flds.length > 0) {
+				for (String fieldInfo : flds) {
+					if (fieldList.contains(fieldInfo))
+						throw duplicateField(classZ,fieldInfo);
+					IocField iocField = new IocField();
+					iocField.setName(fieldInfo);
+					IocValue iocValue = new IocValue();
+					iocValue.setType(IocValue.TYPE_REFER);
+					iocValue.setValue(fieldInfo);
+					iocField.setValue(iocValue);
+					iocObject.addField(iocField);
+					fieldList.add(iocField.getName());
+				}
+			}
+		}
+		if (LOG.isDebugEnabled())
+			LOG.debugf("Processed Ioc Class : %s as [%s]", classZ, beanName);
+	}
+
+	protected IocValue convert(String value) {
+		IocValue iocValue = new IocValue();
+		if (value.indexOf(':') > -1) {
+			iocValue.setType(value.substring(0, value.indexOf(':')));
+			iocValue.setValue(value.substring(value.indexOf(':') + 1));
+		} else
+			iocValue.setValue(value);
+		return iocValue;
+	}
+
+	public String[] getName() {
+		return map.keySet().toArray(new String[map.size()]);
+	}
+
+	public boolean has(String name) {
+		return map.containsKey(name);
+	}
+
+	public IocObject load(IocLoading loading, String name) throws ObjectLoadException {
+		if (has(name))
+			return map.get(name);
+		throw new ObjectLoadException("Object '" + name + "' without define!");
+	}
+	
+	private static final IocException duplicateField(Class<?> classZ, String name){
+		return Lang.makeThrow(IocException.class,	"Duplicate filed defined! Class=%s,FileName=%s",
+				classZ,
+				name);
+	}
+}
--- src/org/nutz/mvc/impl/LoadingsImpl.java
+++ src/org/nutz/mvc/impl/LoadingsImpl.java
@@ -42,7 +42,7 @@ import org.nutz.mvc.annotation.PathMap;
 import org.nutz.mvc.init.module.MainModule;
 import org.nutz.resource.Scans;
 
-public class LoadingsImpl extends Loadings{
+public class LoadingsImpl extends Loadings {
 
 	private static final Log log = Logs.get();
 
@@ -65,7 +65,7 @@ public class LoadingsImpl extends Loadings{
 		evalEncoding(ai, method.getAnnotation(Encoding.class));
 		evalHttpAdaptor(ai, method.getAnnotation(AdaptBy.class));
 		evalActionFilters(ai, method.getAnnotation(Filters.class));
-		evalOk(ai, method.getAnnotation(Ok.class));
+		evalOk(ai, method.getAnnotation(Ok.class), method);
 		evalFail(ai, method.getAnnotation(Fail.class));
 		evalAt(ai, method.getAnnotation(At.class), method);
 		evalActionChainMaker(ai, method.getAnnotation(Chain.class));
@@ -96,12 +96,13 @@ public class LoadingsImpl extends Loadings{
 			// 扫描子包
 			if (scan) {
 				// mawm 为了兼容maven,根据这个type来加载该type所在jar的加载
-				URL location = type.getProtectionDomain().getCodeSource().getLocation();
+				URL location = type.getProtectionDomain().getCodeSource()
+						.getLocation();
 				if (log.isDebugEnabled())
 					log.debugf("module class location '%s'", location);
 				scanModuleInPackageByLocation(location, modules, type);
 
-				//重复扫描,确保能扫描到最可信的类
+				// 重复扫描,确保能扫描到最可信的类
 				scanModuleInPackage(modules, type.getPackage().getName());
 			}
 			// 仅仅加载自己
@@ -118,7 +119,8 @@ public class LoadingsImpl extends Loadings{
 		return modules;
 	}
 
-	protected static void scanModuleInPackage(Set<Class<?>> modules, String packageName) {
+	protected static void scanModuleInPackage(Set<Class<?>> modules,
+			String packageName) {
 		if (log.isDebugEnabled())
 			log.debugf(" > scan '%s'", packageName);
 
@@ -151,8 +153,7 @@ public class LoadingsImpl extends Loadings{
 	 * @param type
 	 */
 	protected static void scanModuleInPackageByLocation(URL classLocation,
-														Set<Class<?>> modules,
-														Class<?> type) {
+			Set<Class<?>> modules, Class<?> type) {
 		String regex = "^.+[.]class$";
 
 		// 1,先扫描jar内的package
@@ -164,7 +165,8 @@ public class LoadingsImpl extends Loadings{
 		if (path.endsWith(".jar")) {
 			// 支队jar进行处理,其他的留给 原程序进行操作
 
-			List<Class<?>> subs = Scans.me().scanPackageInJar(classname, regex, path);
+			List<Class<?>> subs = Scans.me().scanPackageInJar(classname, regex,
+					path);
 
 			checkModule(modules, subs);
 
@@ -175,7 +177,8 @@ public class LoadingsImpl extends Loadings{
 				log.debugf("scanModuleInPackageByJar '%s'", subs);
 		} else {
 			// 3,解决基于maven的工程中,依赖多个工程的情况,因此module的路径是它自身工程的target/classes
-			List<Class<?>> subs = Scans.me().scanPackageInLocation(classname, regex, path);
+			List<Class<?>> subs = Scans.me().scanPackageInLocation(classname,
+					regex, path);
 			checkModule(modules, subs);
 		}
 
@@ -199,6 +202,9 @@ public class LoadingsImpl extends Loadings{
 	}
 
 	public static void evalAt(ActionInfo ai, At at, Class<?> clazz) {
+		/**
+		 * 以Controller后缀的包, 子包处理在controllers包内的 不处理MainModule
+		 */
 		String def = clazz.getSimpleName();
 		if (null != at) {
 			if (null == at.value() || at.value().length == 0) {
@@ -208,25 +214,23 @@ public class LoadingsImpl extends Loadings{
 			}
 			if (!Strings.isBlank(at.key()))
 				ai.setPathKey(at.key());
-		}else{
-			if(clazz.getAnnotation(Modules.class) == null && def.endsWith("Controller")){// 只处理符合规范的控制器
-				String pkg = clazz.getPackage().getName();
-				int start = pkg.indexOf("controllers");
-				if(start != -1){// 严格按照规范的处理子包
-					if(pkg.length() == start+"controllers".length())
-						pkg = "/";
-					else
-						pkg = pkg.substring(start+"controllers".length()).replaceAll(".", "/");
-					String ctrName = Strings.lowerFirst(def.substring(0,def.length()-"Controller".length()));
-					ai.setPaths(Lang.array(pkg + ctrName));
-				}else{// 不按照规范的不鼓励，以默认包为准
-					String ctrName = Strings.lowerFirst(def.substring(0,def.length()-"Controller".length()));
-					ai.setPaths(Lang.array("/"+ctrName));
-				}
+		} else { // create by wangc
+			if (isController(clazz)) {// 只处理符合规范的控制器
+				String moduleName = getContrllerModule(clazz);
+				String ctnName = getControllerName(clazz);
+				if (Strings.isEmpty(moduleName))
+					ai.setPaths(Lang.array("/" + ctnName));
+				else
+					ai.setPaths(Lang.array("/"
+							+ moduleName.replaceAll(".", "/") + "/" + ctnName));
 			}
 		}
 	}
+
 	public static void evalAt(ActionInfo ai, At at, Method method) {
+		/**
+		 * Controller内的需要module.ctnName.methodName MainModule为 methodName
+		 */
 		String def = method.getName();
 		if (null != at) {
 			if (null == at.value() || at.value().length == 0) {
@@ -236,7 +240,16 @@ public class LoadingsImpl extends Loadings{
 			}
 			if (!Strings.isBlank(at.key()))
 				ai.setPathKey(at.key());
-		}else{
+		} else { // create by wangc
+			if (isController(method.getDeclaringClass())) {
+				// 只处理符合规范的module
+				// TODO 这里是不是该把 module的包直接关联起来呢？ 要不然method的url就不能定制了
+				ai.setPaths(Lang.array("/" + Strings.lowerFirst(def)));
+			} else {
+				if (method.getDeclaringClass().getAnnotation(Modules.class) != null) {
+					ai.setPaths(Lang.array("/" + Strings.lowerFirst(def)));
+				}
+			}
 		}
 	}
 
@@ -253,9 +266,40 @@ public class LoadingsImpl extends Loadings{
 		}
 	}
 
-	public static void evalOk(ActionInfo ai, Ok ok,Class<?> clazz) {
+	public static void evalOk(ActionInfo ai, Ok ok, Class<?> clazz) {
 		if (null != ok) {
 			ai.setOkView(ok.value());
+		} else { // create by wangc
+			if (isController(clazz)) {
+				String moduleName = getContrllerModule(clazz);
+				String ctnName = getControllerName(clazz);
+				if (Strings.isEmpty(moduleName))
+					ai.setOkView("jsp:views." + ctnName + ".common");
+				else
+					ai.setOkView("jsp:views." + moduleName + "." + ctnName
+							+ ".common");
+			}
+		}
+	}
+
+	public static void evalOk(ActionInfo ai, Ok ok, Method method) {
+		if (null != ok) {
+			ai.setOkView(ok.value());
+		} else {// create by wangc
+			Class<?> clazz = method.getDeclaringClass();
+			if(isController(clazz)){  
+				String moduleName = getContrllerModule(clazz);
+				String ctnName = getControllerName(clazz);
+				if(Strings.isEmpty(moduleName)){
+					ai.setOkView("jsp:views."+ ctnName+"."+Strings.lowerFirst(method.getName()));
+				}else{
+					ai.setOkView("jsp:views."+moduleName+"."+ctnName+"."+Strings.lowerFirst(method.getName()));
+				}
+			}else{
+				if(clazz.getAnnotation(Modules.class) != null){
+					ai.setOkView("jsp:views."+Strings.lowerFirst(method.getName()));
+				}
+			}
 		}
 	}
 
@@ -266,7 +310,9 @@ public class LoadingsImpl extends Loadings{
 		InjectName innm = type.getAnnotation(InjectName.class);
 		IocBean iocBean = type.getAnnotation(IocBean.class);
 		if (innm == null && iocBean == null) // TODO 再考虑考虑
-			return;
+			if(isController(type)){  // create by wangc
+				beanName = type.getName();
+			}
 		if (iocBean != null) {
 			beanName = iocBean.name();
 		}
@@ -280,10 +326,11 @@ public class LoadingsImpl extends Loadings{
 		ai.setInjectName(beanName);
 	}
 
-	@SuppressWarnings({"unchecked", "rawtypes"})
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void evalActionFilters(ActionInfo ai, Filters filters) {
 		if (null != filters) {
-			List<ObjectInfo<? extends ActionFilter>> list = new ArrayList<ObjectInfo<? extends ActionFilter>>(filters.value().length);
+			List<ObjectInfo<? extends ActionFilter>> list = new ArrayList<ObjectInfo<? extends ActionFilter>>(
+					filters.value().length);
 			for (By by : filters.value()) {
 				list.add(new ObjectInfo(by.type(), by.args()));
 			}
@@ -291,11 +338,11 @@ public class LoadingsImpl extends Loadings{
 		}
 	}
 
-	@SuppressWarnings({"unchecked", "rawtypes"})
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void evalHttpAdaptor(ActionInfo ai, AdaptBy ab) {
 		if (null != ab) {
-			ai.setAdaptorInfo((ObjectInfo<? extends HttpAdaptor>) new ObjectInfo(	ab.type(),
-																					ab.args()));
+			ai.setAdaptorInfo((ObjectInfo<? extends HttpAdaptor>) new ObjectInfo(
+					ab.type(), ab.args()));
 		}
 	}
 
@@ -304,8 +351,10 @@ public class LoadingsImpl extends Loadings{
 			ai.setInputEncoding(org.nutz.lang.Encoding.UTF8);
 			ai.setOutputEncoding(org.nutz.lang.Encoding.UTF8);
 		} else {
-			ai.setInputEncoding(Strings.sNull(encoding.input(), org.nutz.lang.Encoding.UTF8));
-			ai.setOutputEncoding(Strings.sNull(encoding.output(), org.nutz.lang.Encoding.UTF8));
+			ai.setInputEncoding(Strings.sNull(encoding.input(),
+					org.nutz.lang.Encoding.UTF8));
+			ai.setOutputEncoding(Strings.sNull(encoding.output(),
+					org.nutz.lang.Encoding.UTF8));
 		}
 	}
 
@@ -326,14 +375,49 @@ public class LoadingsImpl extends Loadings{
 
 	public static boolean isModule(Class<?> classZ) {
 		int classModify = classZ.getModifiers();
-		if (!Modifier.isPublic(classModify)
-			|| Modifier.isAbstract(classModify)
-			|| Modifier.isInterface(classModify))
+		if (!Modifier.isPublic(classModify) || Modifier.isAbstract(classModify)
+				|| Modifier.isInterface(classModify))
 			return false;
+		if (classZ.getSimpleName().endsWith("Controller")) {
+			// 任何以Controller结尾的都为模块类
+			return true;
+		}
 		for (Method method : classZ.getMethods())
 			if (method.isAnnotationPresent(At.class))
 				return true;
 		return false;
 	}
 
+	public static boolean isController(Class<?> clazz) {
+		return clazz.getAnnotation(Modules.class) == null
+				&& clazz.getSimpleName().endsWith("Controller")
+				&& clazz.getSimpleName().length() > "Controller".length();
+	}
+
+	public static String getControllerName(Class<?> clazz) {
+		String name = clazz.getSimpleName();
+		return Strings.lowerFirst(name.substring(0, name.length()
+				- "Controller".length()));
+	}
+
+	public static String getContrllerModule(Class<?> clazz) {
+		String fullName = clazz.getName();
+		String pkgName = clazz.getPackage().getName();
+		int start = fullName.indexOf("controllers.");
+		if (start == 0) {
+			if ("controllers".equals(pkgName)) {
+				return "";
+			} else {
+				return pkgName.substring("controllers.".length());
+			}
+		} else if (start > 0) {
+			if (start + "controllers".length() == pkgName.length()) {
+				return "";
+			} else {
+				return pkgName.substring(start + "controllers.".length());
+			}
+		} else {// 不符合规范 ,不提供闭包支持
+			return "";
+		}
+	}
 }
