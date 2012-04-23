4701aea8b48a95e261a4284897e8ada39de57c8c
juqkai <juqkai@gmail.com>
2012-04-23 14:57:35 +0800

    调整接口

--- src/org/nutz/json/JsonFilter.java
+++ src/org/nutz/json/JsonFilter.java
@@ -4,7 +4,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 
+ * <p>
+ * 规则:
+ * <ul> 
+ * <li>1. 要定义过滤或是包含, 都直接以对象关联的方式写出, 如: user.name, 
+ * <li>2. 不区分 Map, List 全部都使用 1 中的形式. 基本这里指的 Map, List 是指 JsonCompile 转换的中间对象, 而非 JAVA 属性中的 Map, List. 注意概念
+ * <li>3. 包含还是排除, 以 type 属性做标识, true 为包含, false 为排除.
+ * <li>4. 同一时间只支持一种关系.
+ * </ul>
  * @author juqkai(juqkai@gmail.com)
  */
 public class JsonFilter {
--- test/org/nutz/json/JsonCompileFilterTest.java
+++ test/org/nutz/json/JsonCompileFilterTest.java
@@ -22,14 +22,16 @@ public class JsonCompileFilterTest {
     @Test
     public void includeTest(){
         mates.add("age");
-        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), mates, true);
+        JsonFilter filter = new JsonFilter(mates, true);
+        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), filter);
         assertEquals(1, map.size());
     }
     @Test
     public void includeTest2(){
         mates.add("father.realname");
         mates.add("father.name");
-        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), mates, true);
+        JsonFilter filter = new JsonFilter(mates, true);
+        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), filter);
         Map<?, ?> father = (Map<?, ?>) map.get("father");
         assertEquals(2, father.size());
     }
@@ -37,7 +39,8 @@ public class JsonCompileFilterTest {
     public void excludeTest(){
         mates.add("father.realname");
         mates.add("father.name");
-        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), mates, false);
+        JsonFilter filter = new JsonFilter(mates, false);
+        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/person.txt"), filter);
         Map<?, ?> father = (Map<?, ?>) map.get("father");
         assertEquals(2, father.size());
         assertEquals(69, father.get("age"));
@@ -47,7 +50,8 @@ public class JsonCompileFilterTest {
     @Test
     public void listIncludeTest(){
         mates.add("users.name");
-        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/mateList.txt"), mates, true);
+        JsonFilter filter = new JsonFilter(mates, true);
+        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/mateList.txt"), filter);
         assertEquals(map.size(), 1);
         List<?> users = (List<?>) map.get("users");
         assertEquals(users.size(), 2);
@@ -58,7 +62,8 @@ public class JsonCompileFilterTest {
     @Test
     public void listexcludeTest(){
         mates.add("users.name");
-        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/mateList.txt"), mates, false);
+        JsonFilter filter = new JsonFilter(mates, false);
+        Map<?, ?> map = (Map<?, ?>) Json.fromJson(Streams.fileInr("org/nutz/json/mateList.txt"), filter);
         assertEquals(map.size(), 2);
         List<?> users = (List<?>) map.get("users");
         assertEquals(users.size(), 2);
