169c21b7595333aabef3f25ae4ceeeb44591c3c5
juqkai <juqkai@gmail.com>
2012-04-23 11:50:32 +0800

    调整实现, 添加测试用例

--- src/org/nutz/json/JsonCompileExtend.java
+++ src/org/nutz/json/JsonCompileExtend.java
@@ -7,7 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 扩充 JsonCompile 实现, 以使它支持对JSON字符串的过滤处理
+ * 扩充 JsonCompile 实现, 以使它支持对JSON字符串的过滤处理.
+ * <p>
+ * 规则:
+ * <ul> 
+ * <li>1. 要定义过滤或是包含, 都直接以对象关联的方式写出, 如: user.name, 
+ * <li>2. 不区分 Map, List 全部都使用 1 中的形式. 基本这里指的 Map, List 是指 JsonCompile 转换的中间对象, 而非 JAVA 属性中的 Map, List. 注意概念
+ * <li>3. 包含还是排除, 以 type 属性做标识, true 为包含, false 为排除.
+ * <li>4. 同一时间只支持一种关系.
+ * </ul>
  * @author juqkai(juqkai@gmail.com)
  */
 public class JsonCompileExtend extends JsonCompile{
@@ -39,14 +47,22 @@ public class JsonCompileExtend extends JsonCompile{
      * 包含
      * @return
      */
-    private boolean include(){
-        if(mates == null){
+    private boolean include() {
+        if (mates == null) {
             return true;
         }
         String path = fetchPath();
-        for(String s : mates){
-            if(path.equals(s)){
-                return type ? true : false;
+        for (String s : mates) {
+            if (type) {
+                //包含
+                if (s.startsWith(path)) {
+                    return true;
+                }
+            } else {
+                //排除
+                if (s.equals(path)) {
+                    return false;
+                }
             }
         }
         return type ? false : true;
