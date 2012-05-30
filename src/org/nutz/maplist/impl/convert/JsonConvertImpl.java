e900f4b5e1205e69b7d168aa217bacf2aaf38cc1
juqkai <juqkai@gmail.com>
2012-05-30 13:35:13 +0800

    添加MapList转JSON

---
+++ src/org/nutz/maplist/impl/convert/JsonConvertImpl.java
@@ -0,0 +1,36 @@
+package org.nutz.maplist.impl.convert;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.nutz.json.JsonException;
+import org.nutz.json.JsonFormat;
+import org.nutz.json.impl.JsonRenderImpl;
+import org.nutz.lang.Lang;
+import org.nutz.lang.stream.StringWriter;
+import org.nutz.maplist.MapListConvert;
+
+/**
+ * 将MapList转换成Json
+ * @author juqkai(juqkai@gmail.com)
+ */
+public class JsonConvertImpl implements MapListConvert{
+    private JsonFormat format = null;
+    
+    public JsonConvertImpl() {}
+    public JsonConvertImpl(JsonFormat format) {
+        this.format = format;
+    }
+    
+    public Object convert(Object obj) {
+        StringBuilder sb = new StringBuilder();
+        Writer writer = new StringWriter(sb);
+        try {
+            new JsonRenderImpl(writer, format).render(obj);
+            writer.flush();
+            return sb.toString();
+        } catch (IOException e) {
+            throw Lang.wrapThrow(e, JsonException.class);
+        }
+    };
+}
