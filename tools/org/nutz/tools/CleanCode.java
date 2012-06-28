6dc343f81781aad691942455af01b4818597df8f
Wendal Chen <wendal1985@gmail.com>
2012-06-28 17:08:37 +0800

    ...

--- tools/org/nutz/tools/CleanCode.java
+++ tools/org/nutz/tools/CleanCode.java
@@ -8,27 +8,27 @@ import org.nutz.lang.util.FileVisitor;
 
 public class CleanCode {
 
-	public static void main(String[] args) {
-		CleanCode.removeTabs("src");
-	}
-	
-	public static int removeTabs(String path) {
-		final int[] re = new int[1];
-		Disks.visitFile(new File(path), new FileVisitor() {
-			
-			public void visit(File file) {
-				if (file.isDirectory())
-					return;
-				if (!file.getName().endsWith(".java"))
-					return;
-				String str = Files.read(file);
-				if (!str.contains("\t"))
-					return;
-				str = str.replaceAll("\t", "    ");
-				Files.write(file, str);
-				re[0]++;
-			}
-		}, null);
-		return re[0];
-	}
+    public static void main(String[] args) {
+        CleanCode.removeTabs("tools");
+    }
+    
+    public static int removeTabs(String path) {
+        final int[] re = new int[1];
+        Disks.visitFile(new File(path), new FileVisitor() {
+            
+            public void visit(File file) {
+                if (file.isDirectory())
+                    return;
+                if (!file.getName().endsWith(".java"))
+                    return;
+                String str = Files.read(file);
+                if (!str.contains("\t"))
+                    return;
+                str = str.replaceAll("\t", "    ");
+                Files.write(file, str);
+                re[0]++;
+            }
+        }, null);
+        return re[0];
+    }
 }
