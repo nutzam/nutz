00eee301c2853c99a230bb67851bc5adc4661e56
Wendal Chen <wendal1985@gmail.com>
2012-06-28 16:41:46 +0800

    加入清理代码的类

---
+++ tools/org/nutz/tools/CleanCode.java
@@ -0,0 +1,34 @@
+package org.nutz.tools;
+
+import java.io.File;
+
+import org.nutz.lang.Files;
+import org.nutz.lang.util.Disks;
+import org.nutz.lang.util.FileVisitor;
+
+public class CleanCode {
+
+	public static void main(String[] args) {
+		CleanCode.removeTabs("src");
+	}
+	
+	public static int removeTabs(String path) {
+		final int[] re = new int[1];
+		Disks.visitFile(new File(path), new FileVisitor() {
+			
+			public void visit(File file) {
+				if (file.isDirectory())
+					return;
+				if (!file.getName().endsWith(".java"))
+					return;
+				String str = Files.read(file);
+				if (!str.contains("\t"))
+					return;
+				str = str.replaceAll("\t", "    ");
+				Files.write(file, str);
+				re[0]++;
+			}
+		}, null);
+		return re[0];
+	}
+}
