a506e2dafe2af8f5437cbae81e66c7ebbfa95e48
Wendal Chen <wendal1985@gmail.com>
2012-07-07 11:25:05 +0800

    ...

--- test/org/nutz/mvc/testapp/adaptor/AllAdaptor.java
+++ test/org/nutz/mvc/testapp/adaptor/AllAdaptor.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({SimpleAdaptorTest.class})
 public class AllAdaptor {
 
 }
