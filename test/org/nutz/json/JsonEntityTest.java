f1f696a4edd1106b262e295e8db3e7c8d477319c
Peter Zhang <zozohtnt@gmail.com>
2012-09-14 22:24:48 +0800

    找到一个 JsonEntity 解析的问题

--- test/org/nutz/json/JsonEntityTest.java
+++ test/org/nutz/json/JsonEntityTest.java
@@ -2,10 +2,26 @@ package org.nutz.json;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import org.junit.Test;
+import org.nutz.json.entity.JsonEntity;
 import org.nutz.json.meta.JENObj;
+import org.nutz.lang.Lang;
 
 public class JsonEntityTest {
+
+    @Test
+    public void test_entity_parse() {
+        JsonEntity jen = Json.getEntity(JENObj.class);
+        assertEquals(3, jen.getFields().size());
+
+        assertEquals(long.class, jen.getField("id").getGenericType());
+        assertEquals(String.class, jen.getField("name").getGenericType());
+        assertEquals(int.class, jen.getField("age").getGenericType());
+
+    }
 
     @Test
     public void test_simple_JENObj() {
@@ -23,6 +39,19 @@ public class JsonEntityTest {
         assertEquals(9999999999L, obj.getObjId());
         assertEquals("abc", obj.getName());
         assertEquals(10, obj.getAge());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void test_JENObj_output() {
+        JENObj obj = new JENObj();
+        String str = Json.toJson(obj, JsonFormat.nice().setIgnoreNull(false));
+        Map<String, Object> map = (Map<String, Object>) Json.fromJson(str);
+        String[] keys = map.keySet().toArray(new String[map.size()]);
+        Arrays.sort(keys);
+        String keyStr = Lang.concat(",", keys).toString();
+        assertEquals("age,id,name", keyStr);
+
     }
 
 }
