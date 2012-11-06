9d97c806507e27124e4b72b528c8a2b7cc2c5395
hzl7652 <hzl7652@sina.com>
2012-11-06 18:55:42 +0800

    修改JAVA_HOME TOMCAT_HOME环境变量为本地环境

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,9 @@
 @echo off
-SET TOMCAT_HOME=C:\Program Files\Apache Software Foundation\Tomcat 6.0
+SET TOMCAT_HOME=D:\webapp-server\apache-tomcat-6.0.20
 SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_16
-SET DEPS=D:\home\nutz\deps
+SET DEPS=D:\study-workspace\Nutz-1.b.43\编译源码所需要的文件 1.b.43
 SET OUTPUT=D:\home\nutz\jars
-SET PROJECT_HOME=D:\home\zozoh\workspace\svn\google.nutz\trunk
+SET PROJECT_HOME=D:\study-workspace\nutz
 
 D:
 cd %PROJECT_HOME%\build
