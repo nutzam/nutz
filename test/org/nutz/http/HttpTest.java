eddcdc39142bf0fcccd5c6364942ad3daaac5251
ywjno <ywjno.dev@gmail.com>
2012-12-27 01:44:09 +0800

    调整方法的位置

--- test/org/nutz/http/HttpTest.java
+++ test/org/nutz/http/HttpTest.java
@@ -1,7 +1,6 @@
 package org.nutz.http;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +12,7 @@ public class HttpTest {
 
     @Test
     public void testGet() {
-        Response response = Http.get("http://nutztest.herokuapp.com/get");
+        Response response = Http.get("http://nutztest.herokuapp.com/");
         assertNotNull(response);
         assertNotNull(response.getContent());
         assertNotNull(response.getDetail());
@@ -22,7 +21,19 @@ public class HttpTest {
         assertTrue(response.getStatus() > 0);
         assertNotNull(response.getStream());
     }
-    
+
+    @Test
+    public void testPost() {
+        Map<String, Object> parms = new HashMap<String, Object>();
+        parms.put("version", "NutzTest");
+        parms.put("website", Nutz.version());
+        String response = Http.post("http://nutztest.herokuapp.com/",parms,"utf-8","utf-8");
+        assertNotNull(response);
+        assertTrue(response.length() > 0);
+        // 该post的返回值是"version: #{params[:version]}, website: #{params[:website]}"
+        assertEquals(response, String.format("version: %s, website: %s", "NutzTest", Nutz.version()));
+    }
+
     @Test
     public void testEncode() {
         //根据Http头的Content-Type自动识别编码类型
@@ -33,24 +44,12 @@ public class HttpTest {
         Response response2 = Http.get("www.exam8.com/SiteMap/Article1.htm");
         assertTrue(response2.getContent("GBK").indexOf("考试吧") > 0);
     }
-    
+
     @Test(expected = HttpException.class)
     public void testTimeout() {
     	Response response = Http.get("www.baidu.com", 10 * 1000);
     	assertTrue(response.getStatus() == 200);
     	//抛出超时异常
     	Http.get("www.baidu.com", 1);
-    }
-
-    @Test
-    public void testPost() {
-        Map<String, Object> parms = new HashMap<String, Object>();
-        parms.put("version", "NutzTest");
-        parms.put("website", Nutz.version());
-        String response = Http.post("http://nutztest.herokuapp.com/post",parms,"utf-8","utf-8");
-        assertNotNull(response);
-        assertTrue(response.length() > 0);
-        // 该post的返回值是"version: #{params[:version]}, website: #{params[:website]}"
-        assertEquals(response, String.format("version: %s, website: %s", "NutzTest", Nutz.version()));
     }
 }
