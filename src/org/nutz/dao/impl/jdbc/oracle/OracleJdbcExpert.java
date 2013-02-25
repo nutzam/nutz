c5308311c7183a1878d9c44f6c9c56af02293dc4
Wendal Chen <wendal1985@gmail.com>
2013-02-25 12:08:04 +0800

    fix issue #379 -- @PK增加name的属性

--- src/org/nutz/dao/impl/jdbc/oracle/OracleJdbcExpert.java
+++ src/org/nutz/dao/impl/jdbc/oracle/OracleJdbcExpert.java
@@ -79,13 +79,13 @@ public class OracleJdbcExpert extends AbstractJdbcExpert {
         List<MappingField> pks = en.getPks();
         if (pks.size() > 1) {
             StringBuilder pkNames = new StringBuilder();
-            StringBuilder pkNames2 = new StringBuilder();
             for (MappingField pk : pks) {
                 pkNames.append(pk.getColumnName()).append(',');
-                pkNames2.append(pk.getColumnName()).append('_');
             }
             pkNames.setLength(pkNames.length() - 1);
-            pkNames2.setLength(pkNames2.length() - 1);
+            
+            String pkNames2 = makePksName(en);
+            
             String sql = String.format("alter table %s add constraint primary_key_%s primary key (%s)",
                                        en.getTableName(),
                                        pkNames2,
