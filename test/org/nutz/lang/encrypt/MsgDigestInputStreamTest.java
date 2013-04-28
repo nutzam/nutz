8b80fc7532c941c8afbe6dc53c6331822e7b971b
Peter Zhang <zozohtnt@gmail.com>
2013-04-28 13:36:58 +0800

    去掉单元测试的编译警告

--- test/org/nutz/lang/encrypt/MsgDigestInputStreamTest.java
+++ test/org/nutz/lang/encrypt/MsgDigestInputStreamTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 import org.nutz.lang.Lang;
+import org.nutz.lang.Streams;
 
 public class MsgDigestInputStreamTest {
 
@@ -16,6 +17,7 @@ public class MsgDigestInputStreamTest {
 		ByteArrayInputStream bin = new ByteArrayInputStream(str.getBytes());
 		MsgDigestInputStream in = new MsgDigestInputStream(bin, "md5");
 		in.read(new byte[1024]);
+		Streams.safeClose(in);
 		assertEquals(Lang.md5(str), in.digest());
 	}
 
