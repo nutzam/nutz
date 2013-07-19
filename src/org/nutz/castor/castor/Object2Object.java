bad7a4aed6aa03b5b9341c612a91978129781435
qinerg <qinerg@gmail.com>
2013-07-19 22:36:53 +0800

    如果Obj2Obj转换失败，则采用Json做中介进行终极转换，实现Bean与Bean的对拷

--- src/org/nutz/castor/castor/Object2Object.java
+++ src/org/nutz/castor/castor/Object2Object.java
@@ -2,6 +2,7 @@ package org.nutz.castor.castor;
 
 import org.nutz.castor.Castor;
 import org.nutz.castor.FailToCastObjectException;
+import org.nutz.json.Json;
 import org.nutz.lang.Mirror;
 
 public class Object2Object extends Castor<Object, Object> {
@@ -9,7 +10,11 @@ public class Object2Object extends Castor<Object, Object> {
     @Override
     public Object cast(Object src, Class<?> toType, String... args)
             throws FailToCastObjectException {
-        return Mirror.me(toType).born(src);
+        try {
+            return Mirror.me(toType).born(src);
+        } catch (Exception e) {
+            return Json.fromJson(toType, Json.toJson(src));
+        }
     }
 
 }
