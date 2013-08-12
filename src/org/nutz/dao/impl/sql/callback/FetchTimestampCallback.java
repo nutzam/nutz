0e93af3d88d33693c42821defbee59f0e83416f2
Peter Zhang <zozohtnt@gmail.com>
2013-08-13 01:06:16 +0800

    增加几个新的回调对象

---
+++ src/org/nutz/dao/impl/sql/callback/FetchTimestampCallback.java
@@ -0,0 +1,18 @@
+package org.nutz.dao.impl.sql.callback;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.nutz.dao.sql.Sql;
+import org.nutz.dao.sql.SqlCallback;
+
+public class FetchTimestampCallback implements SqlCallback {
+
+    public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+        if (null != rs && rs.next())
+            return rs.getTimestamp(1);
+        return null;
+    }
+
+}
