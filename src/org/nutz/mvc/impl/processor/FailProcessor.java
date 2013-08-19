af6a9cae0ea8da668fbb088fc906c7b8b8d4ccc9
Peter Zhang <zozohtnt@gmail.com>
2013-08-19 17:56:24 +0800

    当入口函数抛异常的时候，打个简单点的 Log

--- src/org/nutz/mvc/impl/processor/FailProcessor.java
+++ src/org/nutz/mvc/impl/processor/FailProcessor.java
@@ -11,20 +11,22 @@ import org.nutz.mvc.NutConfig;
  * 
  * @author zozoh(zozohtnt@gmail.com)
  * @author wendal(wendal1985@gmail.com)
- *
+ * 
  */
 public class FailProcessor extends ViewProcessor {
-    
+
     private static final Log log = Logs.get();
 
     @Override
     public void init(NutConfig config, ActionInfo ai) throws Throwable {
         view = evalView(config, ai, ai.getFailView());
     }
-    
+
     public void process(ActionContext ac) throws Throwable {
-        if (log.isErrorEnabled())
-            log.error("Catch handle error URI=" + Mvcs.getRequestPath(ac.getRequest()), ac.getError());
+        if (log.isWarnEnabled()) {
+            String uri = Mvcs.getRequestPath(ac.getRequest());
+            log.warnf("Erro@%s : '%s'", uri, ac.getError().toString());
+        }
         super.process(ac);
     }
 }
