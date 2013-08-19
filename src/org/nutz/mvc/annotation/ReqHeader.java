1d0a19e60eeb2f5fefd887284ab3483d04bd5153
wendal <wendal1985@gmail.com>
2013-08-19 12:02:00 +0800

    ...

--- src/org/nutz/mvc/annotation/ReqHeader.java
+++ src/org/nutz/mvc/annotation/ReqHeader.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target({ElementType.PARAMETER})
 @Documented
 public @interface ReqHeader {
 
