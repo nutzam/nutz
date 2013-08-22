8255ac9a5697081e49d5956d8fadac43db9f84bf
wendal <wendal1985@gmail.com>
2013-08-22 11:36:52 +0800

    add: 很久之前写的RawViewTest,一直忘记上传

--- test/org/nutz/mvc/testapp/views/RawViewTest.java
+++ test/org/nutz/mvc/testapp/views/RawViewTest.java
@@ -1,9 +1,20 @@
 package org.nutz.mvc.testapp.views;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.nutz.json.Json;
+import org.nutz.lang.Lang;
 import org.nutz.mvc.testapp.BaseWebappTest;
+import org.nutz.mvc.view.RawView;
+import org.nutz.mvc.view.RawView.RangeRange;
 
 public class RawViewTest extends BaseWebappTest {
 
@@ -23,5 +34,65 @@ public class RawViewTest extends BaseWebappTest {
 
         get("/views/raw5");
         assertTrue(resp.getHeader().get("Content-Type").startsWith("application/json"));
+    }
+    
+//    @Test
+    public void test_raw2() throws Throwable {
+		File src = new File("H://main_qt");
+		File dst = new File("H://cache.tmp");
+		RangeRange rangeRange = new RangeRange(0, src.length());
+//		RawView.writeFileRange(src, new FileOutputStream(dst), rangeRange);
+//
+//		System.out.println(Lang.digest("md5", src));
+//		System.out.println(Lang.digest("md5", dst));
+		
+		List<RangeRange> rs = new ArrayList<RawView.RangeRange>();
+		RawView.parseRange("bytes=0-,-1000000,22222-22222222222", rs, Long.MAX_VALUE);
+		System.out.println(Json.toJson(rs));
+		
+		src = new File("H://raw");
+		FileOutputStream out = new FileOutputStream(src);
+		for (int i = 0; i < 255; i++) {
+			out.write(i);
+		}
+		out.flush();
+		out.close();
+		
+		rs = new ArrayList<RawView.RangeRange>();
+		RawView.parseRange("bytes=0-127", rs, 256);
+		rangeRange = rs.get(0);
+		RawView.writeFileRange(src, new FileOutputStream(dst), rangeRange);
+		System.out.println(dst.length());
+		FileInputStream in = new FileInputStream(dst);
+		for (int i = 0; i < 128; i++) {
+			if (in.read() != i) {
+				System.out.println("ERR");
+			}
+		}
+		
+		
+		rs = new ArrayList<RawView.RangeRange>();
+		RawView.parseRange("bytes=128-", rs, 256);
+		rangeRange = rs.get(0);
+		RawView.writeFileRange(src, new FileOutputStream(dst), rangeRange);
+		in = new FileInputStream(dst);
+		for (int i = 0; i < 128; i++) {
+			if (in.read() != (i + 128)) {
+				System.out.println("ERR");
+			}
+		}
+		
+		rs = new ArrayList<RawView.RangeRange>();
+		RawView.parseRange("bytes=-64", rs, 256);
+		rangeRange = rs.get(0);
+		RawView.writeFileRange(src, new FileOutputStream(dst), rangeRange);
+		in = new FileInputStream(dst);
+		for (int i = 0; i < 64; i++) {
+			if (in.read() != (i + 128 + 64)) {
+				System.out.println("ERR");
+			}
+		}
+		
+		System.out.println("---------------------------END");
     }
 }
