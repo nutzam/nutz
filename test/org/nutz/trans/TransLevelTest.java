5b4cc3b3b4a27f78cb6d19b588dc3156ad7009e8
wendal <wendal1985@gmail.com>
2013-08-22 11:35:53 +0800

    fix: TransLevelTest 不支持derby

--- test/org/nutz/trans/TransLevelTest.java
+++ test/org/nutz/trans/TransLevelTest.java
@@ -157,7 +157,7 @@ public class TransLevelTest extends DaoCase {
     @Test
     public void testReadCommitted() {
         // SqlServer/hsql 在这个测试中，两个线程会相互等待 ...
-        if (dao.meta().isSqlServer() || dao.meta().isHsql()) {
+        if (dao.meta().isSqlServer() || dao.meta().isHsql() || dao.meta().isDerby()) {
             Nutzs.notSupport(dao.meta());
         }
         // H2 会在抛异常：Timeout trying to lock table "TRANS_COMPANY";
