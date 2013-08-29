39dcb53cd7164bdb01a32db4891a2455b060ed56
Peter Zhang <zozohtnt@gmail.com>
2013-08-30 01:40:38 +0800

    修改一下枚举的转换方式，可以支持枚举类型的自定义函数

--- src/org/nutz/castor/castor/Enum2Number.java
+++ src/org/nutz/castor/castor/Enum2Number.java
@@ -8,12 +8,19 @@ import org.nutz.lang.Mirror;
 public class Enum2Number extends Castor<Enum, Number> {
 
     @Override
-    public Number cast(Enum src, Class<?> toType, String... args) throws FailToCastObjectException {
-        Mirror<?> mirror = Mirror.me(Integer.class);
-        Integer re = src.ordinal();
-        if (mirror.canCastToDirectly(toType))
-            return re;
-        return (Number) Mirror.me(toType).born(re.toString());
+    public Number cast(Enum src, Class<?> toType, String... args)
+            throws FailToCastObjectException {
+        Mirror<?> mi = Mirror.me(src);
+
+        // 首先尝试调用枚举对象的 value() 方法
+        try {
+            return (Number) mi.invoke(src, "value");
+        }
+        // 如果失败，就用其顺序号
+        catch (Exception e) {
+            Integer re = src.ordinal();
+            return (Number) Mirror.me(toType).born(re.toString());
+        }
     }
 
 }
--- src/org/nutz/castor/castor/Number2Enum.java
+++ src/org/nutz/castor/castor/Number2Enum.java
@@ -1,6 +1,8 @@
 package org.nutz.castor.castor;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 import org.nutz.castor.Castor;
 import org.nutz.castor.FailToCastObjectException;
@@ -10,23 +12,40 @@ import org.nutz.lang.Lang;
 public class Number2Enum extends Castor<Number, Enum> {
 
     @Override
-    public Enum cast(Number src, Class<?> toType, String... args) throws FailToCastObjectException {
-        try {
-            for (Field field : toType.getFields()) {
-                if (field.getType() == toType) {
-                    Enum em = (Enum) field.get(null);
-                    if (em.ordinal() == src.intValue())
-                        return em;
-                }
-            }
-            throw Lang.makeThrow(    FailToCastObjectException.class,
-                                    "Can NO find enum value in [%s] by int value '%d'",
-                                    toType.getName(),
-                                    src.intValue());
-        }
-        catch (Exception e) {
-            throw Lang.wrapThrow(e, FailToCastObjectException.class);
-        }
-    }
+    public Enum cast(Number src, Class<?> toType, String... args)
+            throws FailToCastObjectException {
+        int v = src.intValue();
+        Enum o = null;
 
+        // 首先试图用采用该类型的 fromInt 的静态方法
+        try {
+            Method m = toType.getMethod("fromInt", int.class);
+            if (Modifier.isStatic(m.getModifiers())
+                && toType.isAssignableFrom(m.getReturnType())) {
+                o = (Enum) m.invoke(null, v);
+            }
+        }
+        catch (Exception e) {}
+
+        // 搞不定，则试图根据顺序号获取
+        if (null == o)
+            try {
+                for (Field field : toType.getFields()) {
+                    if (field.getType() == toType) {
+                        Enum em = (Enum) field.get(null);
+                        if (em.ordinal() == v)
+                            return em;
+                    }
+                }
+                throw Lang.makeThrow(FailToCastObjectException.class,
+                                     "Can NO find enum value in [%s] by int value '%d'",
+                                     toType.getName(),
+                                     src.intValue());
+            }
+            catch (Exception e2) {
+                throw Lang.wrapThrow(e2, FailToCastObjectException.class);
+            }
+
+        return o;
+    }
 }
