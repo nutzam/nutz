7477a8df0e8dfa7bc482dd04b12a071e01bbc46d
wendal <wendal1985@gmail.com>
2013-12-27 09:46:51 +0800

    fix issue #570

--- src/org/nutz/json/Json.java
+++ src/org/nutz/json/Json.java
@@ -257,16 +257,17 @@ public class Json {
     /**
      * 保存所有的 Json 实体
      */
-    private static final ConcurrentHashMap<String, JsonEntity> entities = new ConcurrentHashMap<String, JsonEntity>();
+    @SuppressWarnings("rawtypes")
+	private static final ConcurrentHashMap<Class, JsonEntity> entities = new ConcurrentHashMap<Class, JsonEntity>();
 
     /**
      * 获取一个 Json 实体
      */
     public static JsonEntity getEntity(Mirror<?> mirror) {
-        JsonEntity je = entities.get(mirror.getTypeId());
+        JsonEntity je = entities.get(mirror.getType());
         if (null == je) {
             je = new JsonEntity(mirror);
-            entities.put(mirror.getTypeId(), je);
+            entities.put(mirror.getType(), je);
         }
         return je;
     }
