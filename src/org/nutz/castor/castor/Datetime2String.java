f1e8cab9cd1c80962dd24dbc48f5f116f082689d
belialofking <>
2014-01-16 19:28:16 +0800

    增加日期格式，可以设定日期转换格式

--- src/org/nutz/castor/castor/Datetime2String.java
+++ src/org/nutz/castor/castor/Datetime2String.java
@@ -3,10 +3,20 @@ package org.nutz.castor.castor;
 import org.nutz.lang.Times;
 
 public class Datetime2String extends DateTimeCastor<java.util.Date, String> {
+    private String format = "yyyy-MM-dd HH:mm:ss";
 
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+    
     @Override
     public String cast(java.util.Date src, Class<?> toType, String... args) {
-        return Times.sDT(src);
+        //return Times.sDT(src);
+        return Times.format(format, src);
     }
 
 }
