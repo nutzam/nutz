d0ac033084adbc65df10411463e9d57003621d71
Inside <inside@insidezhou.me>
2014-04-20 00:50:17 +0800

    目的： 让Json支持注入各种JsonRender。
    理由：
    jvm其余上其余语言也有maplist结构，例如scala；通常情况下，scala的maplist很容易与java的maplist相互转换，但nutz的JsonRenderImpl只支持转换Java的maplist。
    
    改动：
    Json由依赖具体的JsonRenderImpl实现改为依赖JsonRender接口。
    因Json.toJson的逻辑而扩展了JsonRender接口，允许注入writer和format。
    根据原有Json.toJson使用JsonRender对象的方式，注入的是JsonRender的Class，在栈内实例化对象并在运行后抛弃。
    同理改了JsonConvertImpl对JsonRenderImpl的强依赖。
    
    AllJson测试，98 passed、1 ignored。

--- src/org/nutz/json/JsonRender.java
+++ src/org/nutz/json/JsonRender.java
@@ -1,6 +1,7 @@
 package org.nutz.json;
 
 import java.io.IOException;
+import java.io.Writer;
 
 /**
  * 对象-->String, 一般就是写入Writer中
@@ -10,4 +11,8 @@ import java.io.IOException;
 public interface JsonRender {
 
     void render(Object obj) throws IOException, JsonException;
+
+    void setWriter(Writer writer);
+
+    void setFormat(JsonFormat jsonFormat);
 }
