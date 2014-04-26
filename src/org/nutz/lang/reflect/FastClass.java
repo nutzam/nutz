141bfeccbd8b17799ffeae6571407c4e355323cf
wendal <wendal1985@gmail.com>
2014-04-26 21:38:10 +0800

    fix issue #633 . 这是一个比较激进的修改,如果在实际项目中报错, 请将disableFastClassInvoker的默认值改成true.

---
+++ src/org/nutz/lang/reflect/FastClass.java
@@ -0,0 +1,22 @@
+package org.nutz.lang.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public interface FastClass {
+
+	String CLASSNAME = "FASTCLASS$$";
+
+	Object invoke(Object obj, Method method, Object... args);
+
+	Object invoke(Object obj, String methodName, Object... args);
+
+	Object born(Constructor<?> constructor, Object... args);
+
+	Object born(Object... args);
+
+	Object setField(Object obj, String fieldName, Object value);
+
+	Object getField(Object obj, String fieldName);
+
+}
