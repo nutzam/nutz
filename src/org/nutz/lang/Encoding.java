b313632a3e593729c6c4207f5fe27e6ac698b118
wendal <wendal1985@gmail.com>
2014-07-17 20:06:44 +0800

    add: 添加几个字符集

--- src/org/nutz/lang/Encoding.java
+++ src/org/nutz/lang/Encoding.java
@@ -5,7 +5,12 @@ import java.nio.charset.Charset;
 public final class Encoding {
 
     public static final String UTF8 = "UTF-8";
-    public static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
+    public static final String GBK = "GBK";
+    public static final String GB2312 = "GB2312";
+
+    public static final Charset CHARSET_UTF8 = Charset.forName(UTF8);
+    public static final Charset CHARSET_GBK = Charset.forName(GBK);
+    public static final Charset CHARSET_GB2312 = Charset.forName(GB2312);
     
     public static String defaultEncoding(){
         return Charset.defaultCharset().name();
