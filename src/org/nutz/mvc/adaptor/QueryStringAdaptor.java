26faf9e53c1ad77d3e6f064f19f91cc2cf3b14c3
Peter Zhang <zozohtnt@gmail.com>
2015-01-29 12:39:43 +0800

    对 HTML5 文件上传给出更多的支持，以及 /** 情况下，默认拼上后缀

---
+++ src/org/nutz/mvc/adaptor/QueryStringAdaptor.java
@@ -0,0 +1,21 @@
+package org.nutz.mvc.adaptor;
+
+import java.lang.reflect.Type;
+
+/**
+ * 普通的 HTTP 请求当调用 req.getParameterValues 时，会读请求体。 这个适配器将只从 QueryString
+ * 读取请求参数，这样对用 HTML5 的上传流，可以不破坏其结构
+ * 
+ * @author zozoh(zozohtnt@gmail.com)
+ */
+public class QueryStringAdaptor extends PairAdaptor {
+
+    @Override
+    protected ParamInjector getNameInjector(String pm,
+                                            String datefmt,
+                                            Class<?> clazz,
+                                            Type[] paramTypes) {
+        return new QueryStringNameInjector(pm, datefmt, clazz, paramTypes);
+    }
+
+}
