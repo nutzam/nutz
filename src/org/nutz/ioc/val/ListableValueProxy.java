9f6beda71f4f0f2023894d22a26844dd993f40a5
wendal <wendal1985@gmail.com>
2015-06-07 20:56:29 +0800

    fixed #335 现在env和sys均支持数组,可选默认值

--- src/org/nutz/ioc/val/ListableValueProxy.java
+++ src/org/nutz/ioc/val/ListableValueProxy.java
@@ -11,41 +11,48 @@ import org.nutz.lang.Lang;
 import org.nutz.lang.LoopException;
 
 public abstract class ListableValueProxy implements ValueProxy {
-    
+
     protected Object obj;
-    
+
     public ListableValueProxy(Object obj) {
         this.obj = obj;
     }
 
     protected abstract Object getValue(String key);
-    
+
     public Object get(IocMaking ing) {
         if (obj == null)
             return null;
-        if (obj.getClass().isArray() || obj instanceof Collection) {
-            final StringBuilder sb = new StringBuilder();
-            Lang.each(obj, new Each<Object>() {
-                public void invoke(int index, Object ele, int length) throws ExitLoop, ContinueLoop, LoopException {
-                    String key = String.valueOf(ele);
-                    if (key.startsWith("!")) {
-                        key = key.substring(1);
-                        Object val = getValue(key);
-                        if (val != null) {
-                            sb.append(val);
-                        }
-                        return;
+        if (obj.getClass().isArray() || obj instanceof Collection) {} else {
+            obj = new Object[]{obj};
+        }
+        final StringBuilder sb = new StringBuilder();
+        Lang.each(obj, new Each<Object>() {
+            public void invoke(int index, Object ele, int length) throws ExitLoop, ContinueLoop, LoopException {
+                String key = String.valueOf(ele);
+                if (key.startsWith("!")) {
+                    key = key.substring(1);
+                    String dft = "";
+                    if (key.contains(":")) {
+                        dft = key.substring(key.indexOf(':') + 1);
+                        key = key.substring(0, key.indexOf(':'));
                     }
                     Object val = getValue(key);
-                    if (val == null) {
-                        sb.append(key);
-                    } else {
+                    if (val != null) {
                         sb.append(val);
+                    } else {
+                        sb.append(dft);
                     }
+                    return;
                 }
-            });
-            return sb.toString();
-        }
-        return getValue(obj.toString());
+                Object val = getValue(key);
+                if (val == null) {
+                    sb.append(key);
+                } else {
+                    sb.append(val);
+                }
+            }
+        });
+        return sb.toString();
     }
 }
