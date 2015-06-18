44eb394c214ca41d332c21bcfb7bdcba93b15057
Peter Zhang <zozohtnt@gmail.com>
2015-06-18 22:00:21 +0800

    去掉编译警告

--- src/org/nutz/lang/reflect/ReflectTool.java
+++ src/org/nutz/lang/reflect/ReflectTool.java
@@ -5,56 +5,67 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.ProtectionDomain;
 
-import org.nutz.lang.Lang;
-
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class ReflectTool {
 
     private static Method DEFINE_CLASS;
     private static final ProtectionDomain PROTECTION_DOMAIN;
-    
+
     static {
         PROTECTION_DOMAIN = getProtectionDomain(ReflectTool.class);
 
         AccessController.doPrivileged(new PrivilegedAction() {
             public Object run() {
                 try {
-                    Class loader = Class.forName("java.lang.ClassLoader"); // JVM crash w/o this
+                    Class loader = Class.forName("java.lang.ClassLoader"); // JVM
+                                                                           // crash
+                                                                           // w/o
+                                                                           // this
                     DEFINE_CLASS = loader.getDeclaredMethod("defineClass",
-                                                            new Class[]{ String.class,
-                                                                         byte[].class,
-                                                                         Integer.TYPE,
-                                                                         Integer.TYPE,
-                                                                         ProtectionDomain.class });
+                                                            new Class[]{String.class,
+                                                                        byte[].class,
+                                                                        Integer.TYPE,
+                                                                        Integer.TYPE,
+                                                                        ProtectionDomain.class});
                     DEFINE_CLASS.setAccessible(true);
-                } catch (ClassNotFoundException e) {
-                    //Lang.impossible();
-                } catch (NoSuchMethodException e) {
-                    //Lang.impossible();
+                }
+                catch (ClassNotFoundException e) {
+                    // Lang.impossible();
+                }
+                catch (NoSuchMethodException e) {
+                    // Lang.impossible();
                 }
                 return null;
             }
         });
     }
-    
+
     public static ProtectionDomain getProtectionDomain(final Class source) {
-        if(source == null) {
+        if (source == null) {
             return null;
         }
-        return (ProtectionDomain)AccessController.doPrivileged(new PrivilegedAction() {
+        return (ProtectionDomain) AccessController.doPrivileged(new PrivilegedAction() {
             public Object run() {
                 return source.getProtectionDomain();
             }
         });
     }
-    
-    public static Class defineClass(String className, byte[] b, ClassLoader loader) throws Exception {
+
+    public static Class defineClass(String className, byte[] b, ClassLoader loader)
+            throws Exception {
         return defineClass(className, b, loader, PROTECTION_DOMAIN);
     }
 
-    public static Class defineClass(String className, byte[] b, ClassLoader loader, ProtectionDomain protectionDomain) throws Exception {
-        Object[] args = new Object[]{className, b, new Integer(0), new Integer(b.length), protectionDomain };
-        Class c = (Class)DEFINE_CLASS.invoke(loader, args);
+    public static Class defineClass(String className,
+                                    byte[] b,
+                                    ClassLoader loader,
+                                    ProtectionDomain protectionDomain) throws Exception {
+        Object[] args = new Object[]{className,
+                                     b,
+                                     new Integer(0),
+                                     new Integer(b.length),
+                                     protectionDomain};
+        Class c = (Class) DEFINE_CLASS.invoke(loader, args);
         // Force static initializers to run.
         Class.forName(className, true, loader);
         return c;
