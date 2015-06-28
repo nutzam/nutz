7d52ffad481f961cef974c11272252dcb09ca427
wendal <wendal1985@gmail.com>
2015-06-28 22:34:21 +0800

    fix #883

--- src/org/nutz/conf/NutzDefaultConfig.js
+++ src/org/nutz/conf/NutzDefaultConfig.js
@@ -6,6 +6,7 @@
 			"org.nutz.el.opt.custom.Min",
 			"org.nutz.el.opt.custom.Trim",
 			"org.nutz.el.opt.custom.MakeUUID",
-			"org.nutz.el.opt.custom.DoBase64"
+			"org.nutz.el.opt.custom.DoBase64",
+			"org.nutz.el.opt.custom.DoURLEncoder"
 		]}
 }
