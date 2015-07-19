2896cdb60dea89922970edad23bf05ca4176f3fe
wendal <wendal1985@gmail.com>
2015-07-19 14:42:33 +0800

    fix #902 且添加一个testcase: HttpTest.test_cookie()

--- test/org/nutz/http/HttpTest.java
+++ test/org/nutz/http/HttpTest.java
@@ -121,4 +121,18 @@ public class HttpTest {
         Response response = Http.get(url);
         System.out.println(response.getContent());
     }
+    
+    @Test
+    public void test_cookie() {
+        Cookie cookie = new Cookie();
+        cookie.setDebug(true);
+        Request req = Request.get("http://115.28.27.32:8090/nutzbook/user/login");
+        Response resp = Sender.create(req).setInterceptor(cookie).send();
+        String expected = cookie.toString();
+        System.out.println(resp.getStatus());
+        System.out.println(Json.toJson(cookie));
+        resp = Sender.create(req).setInterceptor(cookie).send();
+        System.out.println(Json.toJson(cookie)); // 第二次应该没有新的cookie
+        assertEquals(expected, cookie.toString()); // 所以两次toString的结果是一样的, 即同一个session
+    }
 }
