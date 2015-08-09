f4dd1c975238b379c58153c2cdcbaecd425754d9
wendal <wendal1985@gmail.com>
2015-08-10 07:35:01 +0800

    add: 这次能自动发布了

--- mvn_settings.py
+++ mvn_settings.py
@@ -5,46 +5,7 @@ import os.path
 import xml.dom.minidom
 import subprocess
 
-homedir = os.path.expanduser("~")
-
-m2 = xml.dom.minidom.parse(homedir + '/.m2/settings.xml')
-settings = m2.getElementsByTagName("settings")[0]
-
-serversNodes = settings.getElementsByTagName("servers")
-if not serversNodes:
-  serversNode = m2.createElement("servers")
-  settings.appendChild(serversNode)
-else:
-  serversNode = serversNodes[0]
-  
-sonatypeServerNode = m2.createElement("server")
-sonatypeServerId = m2.createElement("id")
-sonatypeServerUser = m2.createElement("username")
-sonatypeServerPass = m2.createElement("password")
-
-idNode = m2.createTextNode("sonatype-nexus-snapshots")
-userNode = m2.createTextNode(os.environ["SONATYPE_USERNAME"])
-passNode = m2.createTextNode(os.environ["SONATYPE_PASSWORD"])
-
-sonatypeServerId.appendChild(idNode)
-sonatypeServerUser.appendChild(userNode)
-sonatypeServerPass.appendChild(passNode)
-
-sonatypeServerNode.appendChild(sonatypeServerId)
-sonatypeServerNode.appendChild(sonatypeServerUser)
-sonatypeServerNode.appendChild(sonatypeServerPass)
-
-serversNode.appendChild(sonatypeServerNode)
-  
-m2Str = m2.toxml()
-f = open(homedir + '/.m2/mySettings.xml', 'w')
-f.write(m2Str)
-f.close()
-
-#print m2Str
-
-print "read to deploy"
 if "1.6.0" in subprocess.check_output("java -Xmx32m -version", shell=1, stderr=subprocess.STDOUT) :
-  subprocess.check_call("mvn clean deploy --settings ~/.m2/mySettings.xml", shell=1, stderr=subprocess.STDOUT)
+  subprocess.check_call("mvn -Dmaven.test.skip=true clean deploy --settings mvn_settings.xml", shell=1, stderr=subprocess.STDOUT)
 else :
   print "not java 6"
