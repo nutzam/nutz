95f3bb7fea1b9e2448deeeb87a4c55453d6cbe90
ywjno <ywjno.dev@gmail.com>
2015-08-17 13:34:53 +0800

    解决当传入的 map 中某个 key 为 null 值的时候，不输出该 key 的内容的错误

--- src/org/nutz/castor/castor/Map2String.java
+++ src/org/nutz/castor/castor/Map2String.java
@@ -5,13 +5,14 @@ import java.util.Map;
 import org.nutz.castor.Castor;
 import org.nutz.castor.FailToCastObjectException;
 import org.nutz.json.Json;
+import org.nutz.json.JsonFormat;
 
 @SuppressWarnings({"rawtypes"})
 public class Map2String extends Castor<Map, String> {
 
     @Override
     public String cast(Map src, Class<?> toType, String... args) throws FailToCastObjectException {
-        return Json.toJson(src);
+        return Json.toJson(src, JsonFormat.tidy());
     }
 
 }
