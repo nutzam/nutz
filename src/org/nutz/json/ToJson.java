48a1df42ed8df0a9d3e5990e4147daf3fffaf110
ywjno <ywjno.dev@gmail.com>
2015-11-07 23:55:32 +0800

    给 ToJson 注解添加 javadoc

--- src/org/nutz/json/ToJson.java
+++ src/org/nutz/json/ToJson.java
@@ -5,8 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 把对象转为Json字符串时调用的方法
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface ToJson {
+
+    /**
+     * 把对象转为Json字符串时调用的方法，默认为调用该对象的『toJson』方法
+     *
+     * @return 对象转为Json字符串时调用的方法
+     */
     String value() default "toJson";
 }
