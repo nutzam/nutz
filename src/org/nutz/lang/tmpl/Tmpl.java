202731603276963c8ca544661dd528f74c38a383
Peter Zhang <zozohtnt@gmail.com>
2015-11-21 15:42:44 +0800

    考虑 Tmpl 的 context 为空的情况

--- src/org/nutz/lang/tmpl/Tmpl.java
+++ src/org/nutz/lang/tmpl/Tmpl.java
@@ -207,6 +207,8 @@ public class Tmpl {
 
     public String render(NutBean context, boolean showKey) {
         StringBuilder sb = new StringBuilder();
+        if (null == context)
+            context = new NutMap();
         for (TmplEle ele : list) {
             ele.join(sb, context, showKey);
         }
