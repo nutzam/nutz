006b41c82584a34f735541dcb6e024fe8703cd28
George Kankava <george.kankava@aurea.com>
2016-01-28 11:35:51 +0400

    squid:EmptyStatementUsageCheck Empty statements should be removed

--- src/org/nutz/dao/util/cri/NumberRange.java
+++ src/org/nutz/dao/util/cri/NumberRange.java
@@ -21,8 +21,7 @@ public abstract class NumberRange extends AbstractSqlExpression {
             for (int i = 0; i < ids.length; i++)
                 sb.append("?,");
             sb.setCharAt(sb.length() - 1, ')');
-        } else
-            ;//OK,无需添加.
+        } //OK,无需添加.
     }
 
     public int joinAdaptor(Entity<?> en, ValueAdaptor[] adaptors, int off) {
