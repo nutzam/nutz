e3e34d05fb8e14a1717489e57e4759b922c0c7fd
wendal chen <wendal1985@gmail.com>
2016-02-05 09:42:29 +0800

    change: 不再需要ActionFilter2接口,而是让ActionFiltersProcessor识别Processor接口

--- src/org/nutz/mvc/impl/processor/ActionFiltersProcessor.java
+++ src/org/nutz/mvc/impl/processor/ActionFiltersProcessor.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import org.nutz.mvc.ActionContext;
 import org.nutz.mvc.ActionFilter;
-import org.nutz.mvc.ActionFilter2;
 import org.nutz.mvc.ActionInfo;
 import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.ObjectInfo;
+import org.nutz.mvc.Processor;
 import org.nutz.mvc.View;
 
 /**
@@ -21,10 +21,9 @@ public class ActionFiltersProcessor extends AbstractProcessor {
 
     protected List<ActionFilter> filters = new ArrayList<ActionFilter>();
     
-    /**
-     * filters2中的顺序与filters是相反的.
-     */
-    protected List<ActionFilter2> filters2 = new ArrayList<ActionFilter2>();
+    protected Processor proxyProcessor;
+    
+    protected Processor lastProcessor;
     
     public void init(NutConfig config, ActionInfo ai) throws Throwable {
         ObjectInfo<? extends ActionFilter>[] filterInfos = ai.getFilterInfos();
@@ -32,17 +31,21 @@ public class ActionFiltersProcessor extends AbstractProcessor {
             for (int i = 0; i < filterInfos.length; i++) {
             	ActionFilter filter = evalObj(config, filterInfos[i]);
                 filters.add(filter);
-                if (filter instanceof ActionFilter2) {
-                	filters2.add(0, (ActionFilter2)filter);
+                if (filter instanceof Processor) {
+            		Processor processor = (Processor)filter;
+                	if (proxyProcessor == null) {
+                		proxyProcessor = processor;
+                		lastProcessor = processor;
+                	} else {
+                		processor.setNext(proxyProcessor);
+                		proxyProcessor = processor;
+                	}
                 }
             }
         }
     }
 
     public void process(ActionContext ac) throws Throwable {
-    	// 总流程参考AbstractMethodInterceptor
-    	
-    	// 前置
         for (ActionFilter filter : filters) {
             View view = filter.match(ac);
             if (null != view) {
@@ -51,21 +54,12 @@ public class ActionFiltersProcessor extends AbstractProcessor {
                 return;
             }
         }
-        try {
+        if (proxyProcessor == null) {
         	doNext(ac);
-        	// 后置
-            for (ActionFilter2 filter2 : filters2) {
-    			filter2.after(ac);
-    		}
-        } catch (Throwable e) {
-        	if (filters2.isEmpty())
-        		throw e;
-        	// 异常
-        	for (ActionFilter2 filter2 : filters2) {
-    			if (filter2.whenError(ac, e))
-    				throw e;
-    		}
+        } else {
+        	if (lastProcessor != null)
+        		lastProcessor.setNext(next);
+        	proxyProcessor.process(ac);
         }
     }
-
 }
