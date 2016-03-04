06c1da3ec55bd73f23a81838d40bf14ae1097ac1
wendal chen <wendal1985@gmail.com>
2016-03-04 10:26:16 +0800

    change: PropertiesProxy的toMap方法应该用LinkedHashMap

--- src/org/nutz/ioc/impl/PropertiesProxy.java
+++ src/org/nutz/ioc/impl/PropertiesProxy.java
@@ -9,7 +9,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -343,7 +343,7 @@ public class PropertiesProxy extends MultiLineProperties {
     }
 
     public Map<String, String> toMap() {
-        return new HashMap<String, String>(this);
+        return new LinkedHashMap<String, String>(this);
     }
     
     public String get(String key) {
