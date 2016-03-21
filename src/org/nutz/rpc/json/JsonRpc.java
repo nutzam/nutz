cda8904221dd17816f93cea1012bf65967077c49
wendal chen <wendal1985@gmail.com>
2016-03-21 13:20:53 +0800

    change: 将jsonrpc移到单独的package

--- src/org/nutz/rpc/json/JsonRpc.java
+++ src/org/nutz/rpc/json/JsonRpc.java
@@ -1,4 +1,4 @@
-package org.nutz.mvc.impl;
+package org.nutz.rpc.json;
 
 import java.io.PrintWriter;
 import java.io.Reader;
@@ -11,10 +11,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.nutz.castor.Castors;
 import org.nutz.http.Request;
-import org.nutz.http.Sender;
-import org.nutz.http.Request.METHOD;
 import org.nutz.http.Response;
+import org.nutz.http.Sender;
 import org.nutz.json.Json;
 import org.nutz.lang.Each;
 import org.nutz.lang.Invoking;
@@ -27,14 +27,14 @@ import org.nutz.lang.util.NutMap;
 /**
  * JSON-RPC 简单封装
  * <p/>
- * <code>@At<p/>@Ok("json")<p/>public NutMap jsonrpc(Reader r){<p/>return JsonRPC.invoke(this);<p/>}</code>
+ * <code>@At<p/>@Ok("json")<p/>public NutMap jsonrpc(Reader r){<p/>return JsonRpc.invoke(r, this, null);<p/>}</code>
  *
  * @author wendal
  * @since 1.r.56
  *
  */
 @SuppressWarnings("unchecked")
-public class JsonRPC {
+public class JsonRpc {
 
     public static final int ParseError = -32700;
     public static final int InvalidRequest = -32600;
@@ -43,21 +43,35 @@ public class JsonRPC {
     public static final int InternalError = -32603;
     public static final int ServerError = -32000;
     public static final String Version = "2.0";
+    public static final String JSONRPC = "jsonrpc";
+    public static final String ERROR = "error";
+    public static final String RESULT = "result";
+    public static final String CODE = "code";
+    public static final String MESSAGE = "message";
+    public static final String METHOD = "method";
+    public static final String ID = "id";
+    public static final String PARAMS = "params";
+    public static final String NAMESPACE = "namespace";
+    public static final String DATA = "data";
+    
+    public static NutMap invoke(Reader r, final Object dft) {
+    	return invoke(r, dft, null);
+    }
 
     /**
      * 服务端
      */
-    public static NutMap invoke(final Object obj, Reader r) {
+    public static NutMap invoke(Reader r, final Object dft, final NutMap namespaces) {
         final NutMap resp = new NutMap();
-        resp.setv("jsonrpc", Version);
+        resp.setv(JSONRPC, Version);
         Object req;
         try {
             req = Json.fromJson(r);
         } catch (Exception e) {
-            return resp.setv("error", error(ParseError, "Parse error", E(e)));
+            return resp.setv(ERROR, error(ParseError, "Parse error", E(e)));
         }
         if (req == null) {
-            return resp.setv("error", error(InvalidRequest, "Invalid Request", "json is null"));
+            return resp.setv(ERROR, error(InvalidRequest, "Invalid Request", "json is null"));
         }
 
         if (req instanceof Iterable) {// rpc批量调用
@@ -65,53 +79,64 @@ public class JsonRPC {
             Lang.each(req, new Each<Object>() {
                 public void invoke(int index, Object ele, int length) {
                     if (ele instanceof Map) {
-                        results.add(JsonRPC.invoke(obj, new NutMap((Map<String, Object>) ele)));
+                        results.add(JsonRpc.invoke(new NutMap((Map<String, Object>) ele), dft, namespaces));
                     } else {
                         NutMap _resp = new NutMap();
-                        _resp.setv("jsonrpc", Version).setv("error", error(InvalidRequest, "Invalid Request", "not map or list"));
+                        _resp.setv(JSONRPC, Version).setv(ERROR, error(InvalidRequest, "Invalid Request", "not map or list"));
                         results.add(_resp);
                     }
                 }
             });
-            return resp.setv("result", results);
+            return resp.setv(RESULT, results);
         } else if (req instanceof Map) { // 单一调用
-            return invoke(obj, new NutMap((Map<String, Object>) req));
+            return invoke(new NutMap((Map<String, Object>) req), dft, namespaces);
         } else { // 传的是什么鸟,拒绝
-            return resp.setv("error", error(InvalidRequest, "Invalid Request", "not map or list"));
+            return resp.setv(ERROR, error(InvalidRequest, "Invalid Request", "not map or list"));
         }
     }
 
-    public static NutMap invoke(Object obj, NutMap req) {
+    public static NutMap invoke(NutMap req, Object dft, NutMap namespaces) {
         NutMap resp = new NutMap();
-        String version = req.getString("jsonrpc", Version);
-        String id = req.getString("id");
-        resp.setv("id", id);
-        resp.setv("jsonrpc", version);
-        String method = req.getString("method");
+        String version = req.getString(JSONRPC, Version);
+        String id = req.getString(ID);
+        resp.setv(ID, id);
+        resp.setv(JSONRPC, version);
+        String method = req.getString(METHOD);
         if (Strings.isBlank(method)) {
-            return resp.setv("error", error(InvalidRequest, "Invalid Request", "method name is blank"));
+            return resp.setv(ERROR, error(InvalidRequest, "Invalid Request", "method name is blank"));
         }
-        List<Object> params = req.getList("params", Object.class, Collections.EMPTY_LIST);
+        String namespace = req.getString(NAMESPACE);
+        Object obj = dft;
+        if (namespace != null) {
+        	if (namespaces == null) {
+        		return resp.setv(ERROR, error(InvalidRequest, "Invalid Request", "namespaces not set at server "));
+        	}
+        	obj = namespaces.get(namespace);
+        }
+        if (obj == null) {
+        	return resp.setv(ERROR, error(InvalidRequest, "Invalid Request", "no such obj"));
+        }
+        List<Object> params = req.getList(PARAMS, Object.class, Collections.EMPTY_LIST);
         Invoking ing;
         try {
             ing = Mirror.me(obj).getInvoking(method, params.toArray());
         } catch (Exception e) {
-            return resp.setv("error", error(MethodNotFound, "Method not found", E(e)));
+            return resp.setv(ERROR, error(MethodNotFound, "Method not found", E(e)));
         }
         try {
-            return resp.setv("result", ing.invoke(obj));
+            return resp.setv(RESULT, ing.invoke(obj));
         } catch (Exception e) {
-            return resp.setv("error", error(ServerError, e.getMessage(), E(e)));
+            return resp.setv(ERROR, error(ServerError, e.getMessage(), E(e)));
         } catch (Throwable e) {
             StringWriter sw = new StringWriter();
             PrintWriter writer = new PrintWriter(sw);
             e.printStackTrace(writer);
-            return resp.setv("error", error(InternalError, e.getMessage(), E(e)));
+            return resp.setv(ERROR, error(InternalError, e.getMessage(), E(e)));
         }
     }
 
     protected static NutMap error(int code, String message, Object data) {
-        return new NutMap().setv("code", code).setv("message", message).setv("data", data);
+        return new NutMap().setv(CODE, code).setv(MESSAGE, message).setv(DATA, data);
     }
 
     protected static String E(Throwable e) {
@@ -133,20 +158,26 @@ public class JsonRPC {
         return (T)Proxy.newProxyInstance(klass.getClassLoader(), new Class<?>[]{klass}, new InvocationHandler() {
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 NutMap jreq = new NutMap();
-                jreq.setv("jsonrpc", "2.0").setv("id", R.UU32()).setv("method", method.getName());
+                jreq.setv(JSONRPC, Version).setv(ID, R.UU32()).setv(METHOD, method.getName());
                 if (!Strings.isBlank(namespace)) {
-                    jreq.put("namespace", namespace);
+                    jreq.put(NAMESPACE, namespace);
                 }
-                jreq.setv("params", args);
-                Request req = Request.create(endpoint, METHOD.POST);
+                jreq.setv(PARAMS, args);
+                Request req = Request.create(endpoint, org.nutz.http.Request.METHOD.POST);
                 req.setData(Json.toJson(jreq));
                 Response resp = Sender.create(req).setTimeout(timeout).send();
                 if (resp.isOK()) {
                     if (method.getReturnType() == Void.class)
                         return null;
-                    return Json.fromJson(method.getGenericReturnType(), resp.getReader());
+                    NutMap re = Json.fromJson(NutMap.class, resp.getReader());
+                    Object error = re.get(ERROR);
+                    if (error != null) {
+                    	throw new JsonRpcException(Json.toJson(error));
+                    }
+                    Object result = re.get(RESULT);
+                    return Castors.me().castTo(result, method.getReturnType());
                 }
-                throw new RuntimeException("resp code="+resp.getStatus());
+                throw new JsonRpcException("resp code="+resp.getStatus());
             }
         });
     }
