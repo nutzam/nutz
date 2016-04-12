512e2599a3357d6d371a623d8f4bf5d99333bbba
wendal chen <wendal1985@gmail.com>
2016-04-12 12:16:45 +0800

    update: 将FilePostSender中生成mulitpart的代码分离出来

--- src/org/nutz/http/sender/FilePostSender.java
+++ src/org/nutz/http/sender/FilePostSender.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -40,58 +41,7 @@ public class FilePostSender extends PostSender {
 			setupDoInputOutputFlag();
 			Map<String, Object> params = request.getParams();
 			if (null != params && params.size() > 0) {
-				final DataOutputStream outs = new DataOutputStream(conn.getOutputStream());
-				for (Entry<String, ?> entry : params.entrySet()) {
-					outs.writeBytes("--" + boundary + SEPARATOR);
-					final String key = entry.getKey();
-					Object val = entry.getValue();
-					if (val == null)
-						val = "";
-					Lang.each(val, new Each<Object>() {
-						@Override
-						public void invoke(int index, Object ele, int length) throws ExitLoop, ContinueLoop, LoopException {
-
-							File f = null;
-							if (ele instanceof File)
-								f = (File) ele;
-							try {
-								if (f != null && f.exists() && f.length() > 0) {
-									outs.writeBytes("Content-Disposition:    form-data;    name=\""
-											+ key
-											+ "\";    filename=\"");
-									outs.write(f.getName().getBytes(request.getEnc()));
-									outs.writeBytes("\"" + SEPARATOR);
-									outs.writeBytes("Content-Type:   application/octet-stream"
-											+ SEPARATOR
-											+ SEPARATOR);
-									InputStream is = null;
-									try {
-										is = Streams.fileIn(f);
-										Streams.write(outs, is);
-										outs.writeBytes(SEPARATOR);
-									}
-									finally {
-										Streams.safeClose(is);
-									}
-								} else {
-									outs.writeBytes("Content-Disposition:    form-data;    name=\""
-											+ key
-											+ "\""
-											+ SEPARATOR
-											+ SEPARATOR);
-									outs.write(String.valueOf(ele).getBytes(request.getEnc()));
-									outs.writeBytes(SEPARATOR);
-								}
-							}
-							catch (Exception e) {
-								throw Lang.wrapThrow(e);
-							}
-						}
-					});
-				}
-				outs.writeBytes("--" + boundary + "--" + SEPARATOR);
-				Streams.safeFlush(outs);
-				Streams.safeClose(outs);
+				export(params, conn.getOutputStream(), boundary, request.getEnc());
 			}
 
 			return createResponse(getResponseHeader());
@@ -99,5 +49,60 @@ public class FilePostSender extends PostSender {
 		} catch (IOException e) {
 			throw new HttpException(request.getUrl().toString(), e);
 		}
+	}
+	
+	public static void export(Map<String, Object> params, OutputStream out, String boundary, final String enc) throws IOException {
+	    final DataOutputStream outs = new DataOutputStream(out);
+        for (Entry<String, ?> entry : params.entrySet()) {
+            outs.writeBytes("--" + boundary + SEPARATOR);
+            final String key = entry.getKey();
+            Object val = entry.getValue();
+            if (val == null)
+                val = "";
+            Lang.each(val, new Each<Object>() {
+                @Override
+                public void invoke(int index, Object ele, int length) throws ExitLoop, ContinueLoop, LoopException {
+
+                    File f = null;
+                    if (ele instanceof File)
+                        f = (File) ele;
+                    try {
+                        if (f != null && f.exists() && f.length() > 0) {
+                            outs.writeBytes("Content-Disposition:    form-data;    name=\""
+                                    + key
+                                    + "\";    filename=\"");
+                            outs.write(f.getName().getBytes(enc));
+                            outs.writeBytes("\"" + SEPARATOR);
+                            outs.writeBytes("Content-Type:   application/octet-stream"
+                                    + SEPARATOR
+                                    + SEPARATOR);
+                            InputStream is = null;
+                            try {
+                                is = Streams.fileIn(f);
+                                Streams.write(outs, is);
+                                outs.writeBytes(SEPARATOR);
+                            }
+                            finally {
+                                Streams.safeClose(is);
+                            }
+                        } else {
+                            outs.writeBytes("Content-Disposition:    form-data;    name=\""
+                                    + key
+                                    + "\""
+                                    + SEPARATOR
+                                    + SEPARATOR);
+                            outs.write(String.valueOf(ele).getBytes(enc));
+                            outs.writeBytes(SEPARATOR);
+                        }
+                    }
+                    catch (Exception e) {
+                        throw Lang.wrapThrow(e);
+                    }
+                }
+            });
+        }
+        outs.writeBytes("--" + boundary + "--" + SEPARATOR);
+        Streams.safeFlush(outs);
+        Streams.safeClose(outs);
 	}
 }
