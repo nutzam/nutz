f71ffedc4b13b5adabcb42233a222cf19c7039a8
wendal chen <wendal1985@gmail.com>
2016-06-03 13:18:44 +0800

    fix: 6770a192d61747390500331c405e9ce9cf21bc1a 导致AllDao里面test fail
    Signed-off-by: wendal chen <wendal1985@gmail.com>

--- src/org/nutz/el/opt/custom/CustomMake.java
+++ src/org/nutz/el/opt/custom/CustomMake.java
@@ -24,17 +24,22 @@ public class CustomMake {
     
     protected static CustomMake me = new CustomMake();
     
+    static {
+        me.init();
+    }
+    
     /**
      * 加载插件
      */
     @SuppressWarnings({ "unchecked", "rawtypes" })
-    public void init(){
+    public CustomMake init(){
         List<String> plug = (List<String>) ((Map)NutConf.get("EL")).get("custom");
         String [] t = plug.toArray(new String[0]);
         PluginManager<RunMethod> rm = new SimplePluginManager<RunMethod>(t);
         for(RunMethod r : rm.gets()){
             me().runms.put(r.fetchSelf(), r);
         }
+        return this;
     }
     
     /**
