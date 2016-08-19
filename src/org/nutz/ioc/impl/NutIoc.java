a591197cd445ceee07cd695def7fd960b5baaa92
wendal chen <wendal1985@gmail.com>
2016-08-19 11:57:01 +0800

    fix: 如果一个ioc bean声明了factory,那么type的声明就没有意义啦
    Signed-off-by: wendal chen <wendal1985@gmail.com>

--- src/org/nutz/ioc/impl/NutIoc.java
+++ src/org/nutz/ioc/impl/NutIoc.java
@@ -194,7 +194,7 @@ public class NutIoc implements Ioc2 {
                         }
 
                         // 修正对象类型
-                        if (null == iobj.getType())
+                        if (null == iobj.getType() && Strings.isBlank(iobj.getFactory()))
                             if (null == type)
                                 throw new IocException(name, "NULL TYPE object '%s'", name);
                             else
