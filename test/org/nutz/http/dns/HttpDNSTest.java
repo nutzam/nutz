4dc89d5dc723b78baeef67fba47c1b55039e9f7d
wendal chen <wendal1985@gmail.com>
2016-10-09 17:12:25 +0800

    ...

---
+++ test/org/nutz/http/dns/HttpDNSTest.java
@@ -0,0 +1,23 @@
+package org.nutz.http.dns;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.nutz.http.Http;
+import org.nutz.http.Response;
+import org.nutz.http.dns.impl.AliDnsProvider;
+
+public class HttpDNSTest {
+
+    @Test
+    public void testSetProvider() {
+        
+        AliDnsProvider provider = new AliDnsProvider("100000");
+        HttpDNS.setProvider(provider);
+        assertNotNull(HttpDNS.getIp("www.aliyun.com"));
+        
+        Response resp = Http.get("http://www.aliyun.com");
+        assertTrue(resp.isOK());
+    }
+
+}
