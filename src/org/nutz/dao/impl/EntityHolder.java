9b37cced19d1ed6f28e67beb0e1e90f200201518
xing kenny <appleplusplus@gmail.com>
2016-10-20 14:22:52 +0800

    这行重复多余了
    在 maker.make(classOfT); 方法中有
    
        holder.set(en); // 保存一下，这样别的实体映射到这里时会用的到
    
    因此此处不需要再put了

--- src/org/nutz/dao/impl/EntityHolder.java
+++ src/org/nutz/dao/impl/EntityHolder.java
@@ -67,7 +67,6 @@ public class EntityHolder {
                 re = map.get(classOfT);
                 if (null == re) {
                     re = maker.make(classOfT);
-                    map.put(classOfT, re);
                 }
             }
         }
