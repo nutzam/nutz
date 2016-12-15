01402ed25970447eace45d8845c8f747149a7cf1
wendal chen <wendal1985@gmail.com>
2016-12-15 11:02:47 +0800

    fixed #1187

--- src/org/nutz/dao/entity/annotation/Comment.java
+++ src/org/nutz/dao/entity/annotation/Comment.java
@@ -1,5 +1,6 @@
 package org.nutz.dao.entity.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,6 +15,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
+@Documented
 public @interface Comment {
     String value() default "";
 }
