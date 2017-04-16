2feed0142477f67a9eaff1ba5e4074c1a07bd052
wendal chen <wendal1985@gmail.com>
2017-04-16 22:30:32 +0800

    update: 下个版本是什么时候呢?

--- src/org/nutz/Nutz.java
+++ src/org/nutz/Nutz.java
@@ -33,7 +33,7 @@ public final class Nutz {
      * @return nutz 项目的版本号
      */
     public static String version() {
-        return String.format("%d.%s.%d",
+        return String.format("%d.%s.%d-SNAPSHOT",
                              majorVersion(),
                              releaseLevel(),
                              minorVersion());
@@ -50,7 +50,7 @@ public final class Nutz {
      * 发布流水
      */
     public static int minorVersion() {
-        return 61;
+        return 62;
     }
 
     /**
