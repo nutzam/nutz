f4969c91d782d9e39eb8466aef2a25f5e43da219
wendal chen <wendal1985@gmail.com>
2017-08-26 19:26:38 +0800

    add: JsonCallback添加自定义对象生成的方法

--- src/org/nutz/json/entity/JsonCallback.java
+++ src/org/nutz/json/entity/JsonCallback.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.io.Writer;
 
 import org.nutz.json.JsonFormat;
-import org.nutz.lang.util.NutMap;
 
 public interface JsonCallback {
     
-    boolean toJson(Object obj, JsonFormat jf, Writer writer, NutMap ctx) throws IOException;
+    Object fromJson(Object obj);
+    
+    boolean toJson(Object obj, JsonFormat jf, Writer writer) throws IOException;
 }
