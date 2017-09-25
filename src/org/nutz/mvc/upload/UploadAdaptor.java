e49375e7515309fce007e9394d842fe21d37cdd8
Peter Zhang <zozohtnt@gmail.com>
2017-09-25 21:24:51 +0800

    去掉编译警告

--- src/org/nutz/mvc/upload/UploadAdaptor.java
+++ src/org/nutz/mvc/upload/UploadAdaptor.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.Map;
 
