8dd07f7bdf4d7baa451fe1daf0a3d819e8d45092
Wendal Chen <wendal1985@gmail.com>
2018-03-28 20:47:12 +0800

    change: JsonTokenScan直接用NutMap

--- src/org/nutz/json/impl/JsonCompileImplV2.java
+++ src/org/nutz/json/impl/JsonCompileImplV2.java
@@ -13,6 +13,7 @@ import org.nutz.json.JsonException;
 import org.nutz.json.JsonParser;
 import org.nutz.lang.Lang;
 import org.nutz.lang.Nums;
+import org.nutz.lang.util.NutMap;
 import org.nutz.mapl.MaplCompile;
 
 /**
@@ -173,7 +174,7 @@ final class JsonTokenScan {
     }
 
     protected Map<String, Object> readMap() {
-        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        Map<String, Object> map = new NutMap();
         boolean hasComma = false;
         OUT: while (true) {
             nextToken();
