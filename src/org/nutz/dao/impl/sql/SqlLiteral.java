34b9520d28e38203074a3fe2cb2f54ad619f6d7d
Wendal Chen <wendal1985@gmail.com>
2018-12-23 22:35:12 +0800

    add: 自定义SQL 支持 ${abc} @{abc} 这种形式
    refer: https://nutz.cn/yvr/t/2emsd1ma36g99rmnqtq6o87skj

--- src/org/nutz/dao/impl/sql/SqlLiteral.java
+++ src/org/nutz/dao/impl/sql/SqlLiteral.java
@@ -146,6 +146,16 @@ public class SqlLiteral implements Cloneable, Serializable {
     }
 
     private static int readTokenName(char[] cs, int i, StringBuilder sb) {
+        // 自定义SQL 支持 ${abc} @{abc} 这种形式
+        if (cs[i+1] == '{') {
+            for (i+=2; i < cs.length; i++) {
+                if (cs[i] == '}')
+                    return i;
+                else
+                    sb.append(cs[i]);
+            }
+            return i;
+        }
         for (++i; i < cs.length; i++) {
             int b = (int) cs[i];
             // Special case for underline ('_')
