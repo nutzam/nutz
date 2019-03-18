3f927bf5f4dc26b5a5618c52cf1945273538c173
Wendal Chen <wendal1985@gmail.com>
2019-03-18 13:28:58 +0800

    release: 1.r.68.v20190318

--- src/org/nutz/Nutz.java
+++ src/org/nutz/Nutz.java
@@ -33,10 +33,7 @@ public final class Nutz {
      * @return nutz 项目的版本号
      */
     public static String version() {
-        return String.format("%d.%s.%d-SNAPSHOT",
-                             majorVersion(),
-                             releaseLevel(),
-                             minorVersion());
+        return "1.r.68.v20190318";
     }
 
     /**
