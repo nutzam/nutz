0e20ed817e617fb1c9e9a3ae473bd2c3251403ac
Wendal Chen <wendal1985@gmail.com>
2019-08-26 21:57:05 +0800

    fix: SharedCollector报NPE

--- src/org/nutz/mvc/impl/contextCollector/SharedCollector.java
+++ src/org/nutz/mvc/impl/contextCollector/SharedCollector.java
@@ -6,20 +6,38 @@ import org.nutz.lang.util.Context;
 import org.nutz.mvc.Mvcs;
 import org.nutz.mvc.ViewContextCollector;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * 共享变量收集器
  */
 public class SharedCollector implements ViewContextCollector {
-    @Override
+    
+    protected List<ViewContextCollector> list;
+    protected boolean hasItem;
+
     public Context collect(HttpServletRequest req, Object obj) {
         Ioc ioc = Mvcs.getIoc();
         Context ctx = Lang.context();
-        String[] names = ioc.getNamesByType(ViewContextCollector.class);
-        for (String name : names) {
-            ViewContextCollector vcc = ioc.get(ViewContextCollector.class, name);
-            ctx.putAll(vcc.collect(req, obj));
+        if (ioc == null)
+            return ctx;
+        if (list == null) {
+            List<ViewContextCollector> tmp = new ArrayList<ViewContextCollector>();
+            String[] names = ioc.getNamesByType(ViewContextCollector.class);
+            for (String name : names) {
+                ViewContextCollector vcc = ioc.get(ViewContextCollector.class, name);
+                tmp.add(vcc);
+                hasItem = true;
+            }
+            list = tmp;
+        }
+        if (hasItem) {
+            for (ViewContextCollector vcc : list) {
+                ctx.putAll(vcc.collect(req, obj));
+            }
         }
         return ctx;
     }
