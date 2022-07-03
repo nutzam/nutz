cfd5c06777ee508928875a3881bcaee8752f76f1
Wendal Chen <wendal1985@gmail.com>
2022-07-03 20:39:16 +0800

    update: Go 1.r.70-SNAPSHOT

--- src/org/nutz/Nutz.java
+++ src/org/nutz/Nutz.java
@@ -33,7 +33,7 @@ public final class Nutz {
      * @return nutz 项目的版本号
      */
     public static String version() {
-        return "1.r.69.v20220703";
+        return "1.r.69-SNAPSHOT";
     }
 
     /**
